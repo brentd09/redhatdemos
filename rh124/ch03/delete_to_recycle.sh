7fd10be44918382055c05daacd64f687fafd631a
BrentD09 <brent.denny@lumifygroup.com>
2024-12-04 07:01:03 +1000

    Create delete_to_recycle.sh

---
+++ rh124/ch03/delete_to_recycle.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+mkdir ~/.Deleted &> /dev/null
+
+delfile() {
+  if [[ -z $1 ]]; then
+    echo "Usage: delfile <filename>"
+    return 1
+  fi  
+  if [[ ! -f $1 ]]; then
+    echo "Error: $1 does not exist"
+    return 2
+  fi
+  mv -f f1 ~/Deleted
+}
