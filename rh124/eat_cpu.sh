2ef9d72c001718ad1e63df4e7ca64de5b203ce65
BrentD09 <brent.denny@lumifygroup.com>
2024-12-04 10:11:24 +1000

    Update eat_cpu.sh

--- rh124/eat_cpu.sh
+++ rh124/eat_cpu.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # loop to consume CPU resources
-for x in {1..10}; do
+for x in {1..4}; do
   for i in {1..1000000}; do
     ((i * i))
     echo -n "$(date +%T) " >> /usr/tmp/eat_cpu.log 
+    sleep 2
   done
 done
 # rm -f /usr/tmp/eat_cpu.log
