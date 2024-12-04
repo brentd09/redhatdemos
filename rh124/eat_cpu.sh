fc3978eee2be1dea64fa616900b119d548243ecb
BrentD09 <brent.denny@lumifygroup.com>
2024-12-04 10:07:24 +1000

    Update eat_cpu.sh

--- rh124/eat_cpu.sh
+++ rh124/eat_cpu.sh
@@ -4,7 +4,7 @@
 for x in {1..10}; do
   for i in {1..1000000}; do
     ((i * i))
-    echo "$(date +%T) " >> /usr/tmp/eat_cpu.log 
+    echo -n "$(date +%T) " >> /usr/tmp/eat_cpu.log 
   done
 done
 # rm -f /usr/tmp/eat_cpu.log
