ace49867b4e06f2c268267216d537c578aaa8b57
BrentD09 <brent.denny@lumifygroup.com>
2025-01-13 16:53:16 +1000

    Update parted_script.sh

--- rh124/ch13/parted_script.sh
+++ rh124/ch13/parted_script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+WHO=$(whoami)
+if [[ "$WHO" != "root" ]];then
+  echo 'USAGE: Please run as root'
+  exit 1
+fi  
+
 # Make sure we are on the right server
 hostnm=$(hostname)
 if [[ $hostnm =~ servera ]]; then
