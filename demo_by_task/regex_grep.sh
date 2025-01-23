b97d1186ed5320ae20c43b37ec9ca966c2a5b1ac
BrentD09 <brent.denny@lumifygroup.com>
2025-01-23 11:33:07 +1000

    Update regex_grep.sh

--- demo_by_task/regex_grep.sh
+++ demo_by_task/regex_grep.sh
@@ -17,3 +17,6 @@ echo ''
 echo -e "\e[36mgrep 're\.' ./linux_benefits.txt\e[0m"
 echo -e "\e[36m-----------------------------\e[0m"
 grep --color=auto 're\.' ./linux_benefits.txt
+echo ''
+echo ''
+echo ''
