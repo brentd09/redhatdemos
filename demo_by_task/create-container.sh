c146dad7cff878447cbfcf538b962f10035b9604
BrentD09 <brent.denny@lumifygroup.com>
2025-01-23 15:03:42 +1000

    Create create-container.sh

---
+++ demo_by_task/create-container.sh
@@ -0,0 +1,26 @@
+ssh student@servera
+sudo -i
+
+dnf install -y podman cockpit-podman cockpit
+systemctl enable --now podman
+
+podman pull registry.access.redhat.com/ubi9/ubi
+
+podman image list
+podman container list
+podman ps
+podman network create mynet
+podman run -d --name ubi-demo-container --network=mynet  -p 8080:80 registry.access.redhat.com/ubi9/ubi  /bin/bash -c 'dnf install -y httpd && echo "PODMAN demo website" > /var/www/html/index.html && httpd -D FOREGROUND' 
+podman ps
+podman stop ubi-demo-container
+
+ 
+cat <<EOF  > Containerfile
+FROM registry.access.redhat.com/ubi9/ubi
+RUN dnf install -y httpd && echo "PODMAN demo website from container" > /var/www/html/index.html
+EXPOSE 80
+CMD ["httpd", "-D", "FOREGROUND"]
+EOF
+
+podman build -t demo_image_rh134 .
+podman run -d -p 8080:80 --name demo_container_rh134 localhost/demo_image_rh134:latest
