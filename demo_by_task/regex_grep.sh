b9fcf0715c524856f90a14f8cee40707f6c9d3c0
BrentD09 <brent.denny@lumifygroup.com>
2025-01-23 11:18:55 +1000

    Create regex_grep.sh

---
+++ demo_by_task/regex_grep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ ! -f ./linux_benefits.txt ]];then 
+  echo "Usage: Move to the directory that contains the linux_benefits.txt file"
+  exit 1
+fi
+
+echo 'grep re. ./linux_benefits.txt'
+echo '-----------------------------'
+grep re. ./linux_benefits.txt
