f0d3c622fe688b4792f2253802c626899641251e
BrentD09 <brent.denny@lumifygroup.com>
2025-01-27 22:10:19 +1000

    Create show-user-group_RH199.sh

---
+++ demo_by_task/user-group/show-user-group_RH199.sh
@@ -0,0 +1,20 @@
+echo id
+id
+
+echo id devops
+id devops
+
+echo ls -ld ~/Downloads
+ls -ld ~/Downloads
+
+echo ps -ef 
+ps -ef 
+
+echo cat /etc/passwd
+cat /etc/passwd
+
+echo cat /etc/group
+cat /etc/group
+
+echo cat /etc/shadow
+cat /etc/shadow
