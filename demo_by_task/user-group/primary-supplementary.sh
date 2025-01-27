f6927540ce1b07809982fc52174355c7e96199f9
BrentD09 <brent.denny@lumifygroup.com>
2025-01-27 23:11:16 +1000

    Update primary-supplementary.sh

--- demo_by_task/user-group/primary-supplementary.sh
+++ demo_by_task/user-group/primary-supplementary.sh
@@ -1,7 +1,8 @@
+# This shows that when a file or directory is created the owner and group of the new file are inherited from the user by default
 sudo mkdir -p /demo/devops-dir
 sudo chmod -R o=rwx /demo/ 
 
-echo touch /demo/devops-dir/demofile
+echo Creating demofile as the devops user
 sudo -u devops touch /demo/devops-dir/demofile
 
 echo list directory and file ownership
