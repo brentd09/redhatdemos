fb343f735b20610cb09a67b5c31807668c458bda
BrentD09 <brent.denny@lumifygroup.com>
2025-01-27 23:21:29 +1000

    Update explain-sudoers.sh

--- demo_by_task/user-group/explain-sudoers.sh
+++ demo_by_task/user-group/explain-sudoers.sh
@@ -1,12 +1,12 @@
-echo In the /etc/sudoers file we see different configurations
-echo This is wha the configuration means
+echo 'In the /etc/sudoers file we see different configurations'
+echo 'This is wha the configuration means'
 echo ''
-echo User assignment
-echo user1     ALL=(ALL:ALL) ALL
+echo 'User assignment'
+echo 'user1     ALL=(ALL:ALL) ALL'
 echo ''
-echo Group assignment
-echo %group1   ALL=(ALL:ALL) ALL
+echo 'Group assignment'
+echo '%group1   ALL=(ALL:ALL) ALL'
 echo ''
-echo Explaining configuration
-echo ALL            =    ( ALL            : ALL              )  ALL
-echo which hosts           Run as user      Run as group        Which commands are allowed to be run as sudo  
+echo 'Explaining configuration'
+echo 'ALL            =    ( ALL            : ALL              )  ALL'
+echo 'Which hosts           Run as user      Run as group        Which commands are allowed to be run as sudo  '
