0e1892691b32131a3b90eb0e4790e717f86c9a7e
BrentD09 <brent.denny@lumifygroup.com>
2025-01-28 08:15:20 +1000

    Update user-admin_rh199.sh

--- demo_by_task/user-group/user-admin_rh199.sh
+++ demo_by_task/user-group/user-admin_rh199.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+echo useradd demouser1
+sudo useradd demouser1
+echo ''
+
+echo Add user to the wheel group
+echo usermod -aG wheel demouser1
+sudo usermod -aG wheel demouser1
+echo ''
+
+echo Determine which groups a user is a member of
+echo id testuser1
+id testuser1
+echo ''
+
+echo Check user account settings
+echo chage -l testuser1
+chage -l testuser1
