05f3c970fe0d6cdf6cd890f4e60237c997e5de65
BrentD09 <brent.denny@lumifygroup.com>
2025-01-27 23:08:27 +1000

    Update primary-supplementary.sh

--- demo_by_task/user-group/primary-supplementary.sh
+++ demo_by_task/user-group/primary-supplementary.sh
@@ -4,8 +4,5 @@ sudo chmod -R o=rwx /demo/
 echo touch /demo/devops-dir/demofile
 sudo -u devops touch /demo/devops-dir/demofile
 
-echo ls -ld /demo/devops-dir
-ls -ld /demo/devops-dir
-
-echo ls -l /demo/devops-dir
-ls -l /demo/devops-dir
+echo list directory and file ownership
+tree -ug /demo
