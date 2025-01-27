7f2f1f22c110b889e8849dcf84622f61c19a1140
BrentD09 <brent.denny@lumifygroup.com>
2025-01-28 08:39:14 +1000

    Update user-admin_rh199.sh

--- demo_by_task/user-group/user-admin_rh199.sh
+++ demo_by_task/user-group/user-admin_rh199.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
 echo useradd demouser1
-sudo useradd demouser1
 read -sp '' promptvar
+sudo useradd demouser1
+echo ''
 
 echo Add user to the wheel group
 echo usermod -aG wheel demouser1
-sudo usermod -aG wheel demouser1
 read -sp '' promptvar
+sudo usermod -aG wheel demouser1
+echo ''
 
 echo Determine which groups a user is a member of
 echo id demouser1
-id demouser1
 read -sp '' promptvar
+id demouser1
+echo ''
 
 echo Check user account settings
 echo chage -l demouser1
-sudo chage -l demouser1
 read -sp '' promptvar
+sudo usermod -aG wheel demouser1
+sudo chage -l demouser1
+echo ''
+
 
 echo Configure 
 echo Max Password Age: 40 days
@@ -27,4 +33,6 @@ echo Number of Days Before Expiry That Warnings Start: 7 days
 echo Date When Account Will be Disabled: 2027-12-30
 echo ''
 echo chage -m 2 -M 40 -I 3 -W 7 -E '2027-12-30' demouser1
+read -sp '' promptvar
 sudo chage -m 2 -M 40 -I 3 -W 7 -E '2027-12-30' demouser1
+echo ''
