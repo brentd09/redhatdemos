44c2e1cfeb51ee0df8115990aa8ece021ecbe8af
BrentD09 <brent.denny@lumifygroup.com>
2025-01-28 17:12:50 +1000

    Update sebooleans_rh199.sh

--- demo_by_task/selinux/sebooleans_rh199.sh
+++ demo_by_task/selinux/sebooleans_rh199.sh
@@ -4,6 +4,7 @@ echo Check selinux booleans
 echo getsebool -a
 read -sp '' promptvar
 getsebool -a
+echo ''
 echo One of these booleans can allow the httpd web server to access home directories
 getsebool -a | grep httpd_enable_homedirs
 echo ''
