95346fab3dd8366dcd592d79e3b4fb796e9429a8
BrentD09 <brent.denny@lumifygroup.com>
2025-01-29 09:31:32 +1000

    Update eat-disk_rh199.sh

--- demo_by_task/process/eat-disk_rh199.sh
+++ demo_by_task/process/eat-disk_rh199.sh
@@ -11,9 +11,8 @@ mkdir -p $DIRECTORY
 # Generate files with random data
 for i in $(seq 1 $FILE_COUNT); do
     # Create a large file by writing random data from /dev/urandom
-    dd if=/dev/urandom of=$DIRECTORY/file$i.bin bs=1M count=$FILE_SIZE_MB
+    dd if=/dev/urandom of=$DIRECTORY/file$i.bin bs=1M count=$FILE_SIZE_MB *> /dev/null
     # Alternatively, you can use /dev/zero for zeroed files:
     # dd if=/dev/zero of=$DIRECTORY/file$i.bin bs=1M count=$FILE_SIZE_MB
 done
 
-echo "Created $FILE_COUNT files of size ${FILE_SIZE_MB}MB in $DIRECTORY."
