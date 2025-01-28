aeaec62238b0952ed9ea1db4c49626e2ab7c5b75
BrentD09 <brent.denny@lumifygroup.com>
2025-01-28 10:05:59 +1000

    Create swap-primary-group_rh199.sh

---
+++ demo_by_task/user-group/swap-primary-group_rh199.sh
@@ -0,0 +1,3 @@
+echo newgrp wheel student
+echo This actully forks a new shell with the wheel group as the primary
+echo If you exit this new shell you will find that the parent shell groups have not been changed
