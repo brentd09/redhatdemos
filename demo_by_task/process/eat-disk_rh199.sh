354df869a518d16ec5c179cc1050df0e9634f7c5
BrentD09 <brent.denny@lumifygroup.com>
2025-01-29 09:27:44 +1000

    Update eat-disk_rh199.sh

--- demo_by_task/process/eat-disk_rh199.sh
+++ demo_by_task/process/eat-disk_rh199.sh
@@ -5,15 +5,15 @@ FILE_COUNT=100
 FILE_SIZE_MB=50
 DIRECTORY="./disk_io_test"
 
-# Create directory to store files
+# Create a directory to store files
 mkdir -p $DIRECTORY
 
-# Generate files
+# Generate files with random data
 for i in $(seq 1 $FILE_COUNT); do
-    # Create a large file by writing random data
-    fallocate -l ${FILE_SIZE_MB}M $DIRECTORY/file$i.bin
-    # Alternatively, you can use:
-    # head -c ${FILE_SIZE_MB}M </dev/urandom > $DIRECTORY/file$i.bin
+    # Create a large file by writing random data from /dev/urandom
+    dd if=/dev/urandom of=$DIRECTORY/file$i.bin bs=1M count=$FILE_SIZE_MB
+    # Alternatively, you can use /dev/zero for zeroed files:
+    # dd if=/dev/zero of=$DIRECTORY/file$i.bin bs=1M count=$FILE_SIZE_MB
 done
 
 echo "Created $FILE_COUNT files of size ${FILE_SIZE_MB}MB in $DIRECTORY."
