4ffb952d82162e9807d30e700513a3b807c64699
BrentD09 <brent.denny@lumifygroup.com>
2025-01-28 10:10:57 +1000

    Update swap-primary-group_rh199.sh

--- demo_by_task/user-group/swap-primary-group_rh199.sh
+++ demo_by_task/user-group/swap-primary-group_rh199.sh
@@ -2,7 +2,7 @@
 echo newgrp wheel student
 echo This actully forks a new shell with the wheel group as the primary
 echo If you exit this new shell you will find that the parent shell groups have not been changed
-echo ***After executing this command type id the see the group change and then exit to return to the parent shell***
+echo *After executing this command type "id" to see the group change and then "exit" to return to the parent shell*
 read -sp '' promptvar
 newgrp wheel student
 echo ''
