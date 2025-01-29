cdad71c7d5bd494279ed94583f80cc0a4f9f61b5
BrentD09 <brent.denny@lumifygroup.com>
2025-01-29 12:42:21 +1000

    Update calc-real-load-average_rh199.sh

--- demo_by_task/process/calc-real-load-average_rh199.sh
+++ demo_by_task/process/calc-real-load-average_rh199.sh
@@ -4,7 +4,7 @@ numsock=$(lscpu | grep Socket | awk '{print $NF}')
 numcore=$(lscpu | grep Core | awk '{print $NF}')
 numcpus=$((numsock * numcore))
 uptime=$(uptime )
-loadave=$(awk -F, '{print $3", "$4", "$5}' <<< "$uptime" )
-realave=$(awk -v numcpus="$numcpus" '{print $3 / numcpus ", " $4 / numcpus ", " $5 / numcpus }' <<< "$uptime" )
-echo $loadave
-echo "real average: $realave
+loadave=$(awk '{print $(NF-2) $(NF-1) $NF}' <<< "$uptime" | awk '{print $1 $2 $3}')
+realave=$(awk -v numcpus="$numcpus" '{print $1 / numcpus ", " $2 / numcpus ", " $3 / numcpus }' <<< "$loadave" )
+echo "load average: $loadave"
+echo "real average: $realave"
