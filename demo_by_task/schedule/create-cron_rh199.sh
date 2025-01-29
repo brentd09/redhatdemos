c17743a2faa5bfe2ce5bdafeae3597d504914018
BrentD09 <brent.denny@lumifygroup.com>
2025-01-29 14:30:16 +1000

    Create create-cron_rh199.sh

---
+++ demo_by_task/schedule/create-cron_rh199.sh
@@ -0,0 +1,6 @@
+echo crontab files are in this format
+echo Minutes Hours Day-of-Month Month Day-of-Week Command
+echo ------- ----- ------------ ----- ----------- -------
+echo ''
+
+echo Run a command 
