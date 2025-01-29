5d85e16d3d9d3e43cf8a3758242794b075b19e10
BrentD09 <brent.denny@lumifygroup.com>
2025-01-29 19:24:57 +1000

    Create make-cron-daily_rh199.sh

---
+++ demo_by_task/schedule/make-cron-daily_rh199.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo Create a script in /etc/cron.daily/log-datetime.sh
+sudo echo '#!/bin/bash' > /etc/cron.daily/log-datetime.sh
+sudo echo '' >> etc/cron.daily/log-datetime.sh
+sudo echo 'for a in 1 2 3 4 5; do' >> /etc/cron.daily/log-datetime.sh
+sudo echo '  datew >> /usr/tmp/my_date.log' >> /etc/cron.daily/log-datetime.sh
+sudo echo '  sleep 2' >> /etc/cron.daily/log-datetime.sh
+sudo echo 'done' >> /etc/cron.daily/log-datetime.sh
+
+chmod 777 /etc/cron.daily/log-datetime.sh
+
