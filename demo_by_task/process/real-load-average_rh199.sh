a93a4e3b331cf8a90de2538d9f19299426e8ed01
BrentD09 <brent.denny@lumifygroup.com>
2025-01-29 13:07:18 +1000

    Update real-load-average_rh199.sh

--- demo_by_task/process/real-load-average_rh199.sh
+++ demo_by_task/process/real-load-average_rh199.sh
@@ -4,7 +4,7 @@ numsock=$(lscpu | grep Socket | awk '{print $NF}')
 numcore=$(lscpu | grep Core | awk '{print $NF}')
 numcpus=$((numsock * numcore))
 uptime=$(uptime )
-loadave=$(awk '{print $(NF-2) $(NF-1) $NF}' <<< "$uptime" | awk -F, '{print $1", "$2", "$3}')
+loadave=$(awk '{print $(NF-2) $(NF-1) $NF}' <<< "$uptime" | awk -F, '{print $1",  "$2",  "$3}')
 realave=$(awk -F, -v numcpus="$numcpus" '{print $1 / numcpus "," $2 / numcpus "," $3 / numcpus }' <<< "$loadave" )
 realave=$(awk -F, -v numcpus="$numcpus" '{printf "%.2f,  %.2f,  %.2f\n", $1 / numcpus , $2 / numcpus,  $3 / numcpus }' <<< "$loadave" )
 echo "Number of CPUs: $numcpus"
