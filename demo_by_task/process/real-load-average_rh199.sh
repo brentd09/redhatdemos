cf9d5095540c150cce16063f9214bdac38c74552
BrentD09 <brent.denny@lumifygroup.com>
2025-01-29 13:00:10 +1000

    Update real-load-average_rh199.sh

--- demo_by_task/process/real-load-average_rh199.sh
+++ demo_by_task/process/real-load-average_rh199.sh
@@ -8,5 +8,6 @@ loadave=$(awk '{print $(NF-2) $(NF-1) $NF}' <<< "$uptime" | awk '{print $1 $2 $3
 realave=$(awk -F, -v numcpus="$numcpus" '{print $1 / numcpus "," $2 / numcpus "," $3 / numcpus }' <<< "$loadave" )
 realave=$(awk -F, -v numcpus="$numcpus" '{printf "%.2f,%.2f,%.2f\n", $1 / numcpus , $2 / numcpus,  $3 / numcpus }' <<< "$loadave" )
 echo "Number of CPUs: $numcpus"
+echo "Average for   : 1min  5min  15min"
 echo "load average  : $loadave"
 echo "real average  : $realave"
