59f4570b121320d3616c9145b91a9e4bbf63aa5b
BrentD09 <brent.denny@lumifygroup.com>
2025-01-31 09:47:03 +1000

    Update net-stress_rh199.sh

--- demo_by_task/process/net-stress_rh199.sh
+++ demo_by_task/process/net-stress_rh199.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for a in $(seq 100000);do 
-  curl https://raw.githubusercontent.com/openethereum/wordlist/refs/heads/master/res/wordlist.txt >> /dev/null
+  curl https://raw.githubusercontent.com/openethereum/wordlist/refs/heads/master/res/wordlist.txt &> /dev/null
 done  
