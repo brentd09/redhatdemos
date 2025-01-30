301599c5b73605c3a3b6b43f8e20d9161d969d22
BrentD09 <brent.denny@lumifygroup.com>
2025-01-30 15:52:46 +1000

    Update realtime-process_rh199.sh

--- demo_by_task/process/realtime-process_rh199.sh
+++ demo_by_task/process/realtime-process_rh199.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+me=$(whoami)
+if [[ $me != 'root' ]]; then
+  echo 'Usage: This script must be run as root'
+  exit 1
+fi
+
 echo Run a process as a real time priority
 echo 'chrt -f 21 sleep 1000 &'
 sudo chrt -f 21 sleep 1000 &
