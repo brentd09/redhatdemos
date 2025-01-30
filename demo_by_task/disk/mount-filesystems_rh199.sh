a030c63371021fe5b73faef1295fd3a28f713ea8
BrentD09 <brent.denny@lumifygroup.com>
2025-01-30 17:22:55 +1000

    Update mount-filesystems_rh199.sh

--- demo_by_task/disk/mount-filesystems_rh199.sh
+++ demo_by_task/disk/mount-filesystems_rh199.sh
@@ -22,19 +22,51 @@ if [[ $part1result -ne 0 ||  $part2result -ne 0 ]]; then
   exit 1
 fi
 
+echo 'mkdir -p /mnt/salesdata'
+read -sp '' promptvar
 mkdir -p /mnt/salesdata
+echo ''
+
+echo 'mkdir -p /mnt/hrdata'
+read -sp '' promptvar
 mkdir -p /mnt/hrdata
+echo ''
 
+echo 'mount /dev/vdc1 /mnt/salesdata'
+read -sp '' promptvar
 mount /dev/vdc1 /mnt/salesdata
+echo ''
+
+echo 'mount /dev/vdc2 /mnt/hrdata'
+read -sp '' promptvar
 mount /dev/vdc2 /mnt/hrdata
+echo ''
 
+echo 'df
+read -sp '' promptvar
 df
+echo ''
 
+echo 'touch /mnt/salesdata/sales.db'
+read -sp '' promptvar
 touch /mnt/salesdata/sales.db
+echo ''
+echo 'touch /mnt/hrdata/hr.db'
+read -sp '' promptvar
 touch /mnt/hrdata/hr.db
+echo ''
 
+echo 'ls /mnt/salesdata/ /mnt/hrdata/'
+read -sp '' promptvar
 ls /mnt/salesdata/ /mnt/hrdata/
+echo ''
 
+echo 'umount /mnt/salesdata/ /mnt/hrdata/'
+read -sp '' promptvar
 umount /mnt/salesdata/ /mnt/hrdata/
+echo ''
 
+echo 'ls /mnt/salesdata/ /mnt/hrdata/'
+read -sp '' promptvar
 ls /mnt/salesdata/ /mnt/hrdata/
+echo ''
