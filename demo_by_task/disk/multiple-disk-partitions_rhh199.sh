90458f4deb3382e131d967c461e2133cd7514e7f
BrentD09 <brent.denny@lumifygroup.com>
2025-01-30 16:38:27 +1000

    Update multiple-disk-partitions_rhh199.sh

--- demo_by_task/disk/multiple-disk-partitions_rhh199.sh
+++ demo_by_task/disk/multiple-disk-partitions_rhh199.sh
@@ -10,11 +10,11 @@ fi
 hostnm=$(hostname)
 if [[ $hostnm =~ servera ]]; then
   echo 'This will create 3 partitions on /dev/vdb and 3 partitions on /dev/vdc'
-  echo '/dev/vdb1 - 100MB, /dev/vdb2 - 200MB, /dev/vdb3 - 300MB'
-  echo '/dev/vdc1 - 500MB, /dev/vdc2 - 600MB, /dev/vdc3 - 700MB'
-  DISKS="/dev/vdb /dev/vdc"
+  echo '/dev/vdc1 - 100MB, /dev/vdc2 - 200MB, /dev/vdc3 - 300MB'
+  echo '/dev/vdd1 - 500MB, /dev/vdd2 - 600MB, /dev/vdd3 - 700MB'
+  DISKS="/dev/vdc /dev/vdd"
   for DISK in $DISKS; do 
-    if [[ $DISK == /dev/vdb ]];then
+    if [[ $DISK == /dev/vdc ]];then
       partbgn1='0MB'; partend1='100MB'; partbgn2='101MB'; partend2='300MB'; partbgn3='301MB'; partend3='600MB'
     else 
       partbgn1='0MB'; partend1='500MB'; partbgn2='501MB'; partend2='1100MB'; partbgn3='1101MB'; partend3='1800MB'
