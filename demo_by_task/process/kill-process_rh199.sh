d48ce6c59b18d84bf42a064a9c7b19867e011f35
BrentD09 <brent.denny@lumifygroup.com>
2025-01-31 10:55:15 +1000

    Update kill-process_rh199.sh

--- demo_by_task/process/kill-process_rh199.sh
+++ demo_by_task/process/kill-process_rh199.sh
@@ -7,5 +7,11 @@ sudo -u devops sleep 10000 & &> /dev/null
 sudo -u devops sleep 10000 & &> /dev/null
 
 echo 'ps -ef | grep devops'
-
+read -sp '' promptvar
 ps -ef | grep '^devops'
+echo ''
+
+echo 'pgrep -u devops'
+read -sp '' promptvar
+pgrep -u devops
+echo ''
