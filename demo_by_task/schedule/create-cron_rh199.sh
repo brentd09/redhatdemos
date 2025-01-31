f3f1ce56eebc224c39564218243c59c79f6bc5c5
BrentD09 <brent.denny@lumifygroup.com>
2025-01-31 14:08:30 +1000

    Update create-cron_rh199.sh

--- demo_by_task/schedule/create-cron_rh199.sh
+++ demo_by_task/schedule/create-cron_rh199.sh
@@ -6,6 +6,17 @@ echo ''
 echo Run a command at 1:30pm on Tuesday and Thursday
 # Define the cron job
 CRON_JOB="30 13 * * 2,4 sleep 10"
+# Add the cron job to the crontab
+( crontab -l; echo "$CRON_JOB" ) | crontab -
 
+echo Run a command every minute stating at 3:00am until 3:59am 
+# Define the cron job
+CRON_JOB="*/1 3 * * * sleep 20"
+# Add the cron job to the crontab
+( crontab -l; echo "$CRON_JOB" ) | crontab -
+
+echo Run a command every minute stating at 1:00am until 7:59am and then 9:00pm until 9:59pm  
+# Define the cron job
+CRON_JOB="*/1 1-7,21 * * * sleep 30"
 # Add the cron job to the crontab
 ( crontab -l; echo "$CRON_JOB" ) | crontab -
