40047cf2a637f66f30f32432601b9512abe1fe17
BrentD09 <brent.denny@lumifygroup.com>
2025-01-31 23:53:46 +1000

    Create delete-tmpfiles_rh199.sh

---
+++ demo_by_task/schedule/delete-tmpfiles_rh199.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo '# Store this file as /etc/tmpfiles.d/myapp.conf' > /etc/tmpfiles.d/myapp.conf
+echo '# Create a directory with 755 mode' >> /etc/tmpfiles.d/myapp.conf
+echo 'd /var/tmp/myapp 755 root root -' >> /etc/tmpfiles.d/myapp.conf
+echo '' >> /etc/tmpfiles.d/myapp.conf
+echo '# Create a temporary file with 600 mode' >> /etc/tmpfiles.d/myapp.conf
+echo 'f /var/tmp/myapp/createdfile.txt 600 root root -'' >> /etc/tmpfiles.d/myapp.conf
+echo '' >> /etc/tmpfiles.d/myapp.conf
+echo '# Remove files that have not been accessed in 1 min' >> /etc/tmpfiles.d/myapp.conf
+echo 'x /var/tmp/myapp 755 - - -' >> /etc/tmpfiles.d/myapp.conf
+
+echo 'cat /etc/tmpfiles.d/myapp.comf'
+read -sp '' promptvar
+cat /etc/tmpfiles.d/myapp.comf
+echo ''
+
+echo 'Check to see if the myapp directory exists, it will probably not exist yet'
+echo 'tree /usr/tmp'
+read -sp '' promptvar
+tree /usr/tmp
+echo ''
+
+echo 'Have systemd-tmpfiles create the files and directories we need'
+echo 'systemd-tmpfiles --create'
+read -sp '' promptvar
+systemd-tmpfiles --create
+echo ''
+
+echo 'Add a file to the /usr/tmp/myapp directory'
+echo 'touch /usr/tmp/myapp/demofile.txt'
+read -sp '' promptvar
+touch /usr/tmp/myapp/demofile.txt
+tree /usr/tmp/myapp/
+echo ''
+
+echo 'Wait 90 seconds'
+echo 'sleep 90'
+read -sp '' promptvar
+sleep 90
+echo ''
+
+echo 'Check the tmp directory again, demofile.txt should be deleted'
+echo 'echo 'tree /usr/tmp'
+read -sp '' promptvar
+echo 'tree /usr/tmp
