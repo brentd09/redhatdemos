25a12006fb17d0467767fdd43b9926f69bdf8329
BrentD09 <brent.denny@lumifygroup.com>
2025-01-31 14:17:02 +1000

    Update create-cron_rh199.sh

--- demo_by_task/schedule/create-cron_rh199.sh
+++ demo_by_task/schedule/create-cron_rh199.sh
@@ -29,5 +29,5 @@ echo  '|  |  |  .------- month (1 - 12) OR jan,feb,mar,apr ...'
 echo  '|  |  |  |  .---- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat'
 echo  '|  |  |  |  |'
 echo  '*  *  *  *  * user-name command to be executed'
-cat /etc/crontab
+echo ''
 crontab -l
