78742b84396e88a16df494d0d756833848a4fb85
BrentD09 <brent.denny@lumifygroup.com>
2025-02-03 16:47:06 +1000

    Update extend-lvm_rh199.sh

--- demo_by_task/lvm/extend-lvm_rh199.sh
+++ demo_by_task/lvm/extend-lvm_rh199.sh
@@ -9,7 +9,37 @@ fi
 # Make sure we are on the right server
 hostnm=$(hostname)
 if [[ $hostnm =~ servera ]]; then
-  
+
+  echo 'parted /dev/vdc --script mkpart lvmextend etx4 601MB 3000MB'
+  read -sp '' promptvar
+  parted /dev/vdc --script mkpart lvmextend etx4 601MB 3000MB
+  echo ''
+
+  echo 'pvcreate /dev/vdc4'
+  read -sp '' promptvar
+  pvcreate /dev/vdc4
+  echo ''
+
+  echo 'vgextend vg1_demo /dev/vdc4'
+  read -sp '' promptvar
+  vgextend vg1_demo /dev/vdc4
+  echo ''
+
+  echo 'lvextend -L +2GB /dev/vg1_demo/lv2_demo'
+  read -sp '' promptvar
+  lvextend -L +2GB /dev/vg1_demo/lv2_demo
+  echo ''
+
+  echo 'resize2fs /dev/vg1_demo/lv2_demo'
+  read -sp '' promptvar
+  resize2fs /dev/vg1_demo/lv2_demo
+  echo ''
+
+  echo 'df'
+  read -sp '' promptvar
+  df
+  echo ''
+
 else
   echo 'Usage: Needs to be run on servera as root'
 fi
