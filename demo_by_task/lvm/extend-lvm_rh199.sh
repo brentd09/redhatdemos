a0c2b2a73f893c2f802527e38b1d1149d50c689c
BrentD09 <brent.denny@lumifygroup.com>
2025-02-03 16:32:51 +1000

    Create extend-lvm_rh199.sh

---
+++ demo_by_task/lvm/extend-lvm_rh199.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WHO=$(whoami)
+if [[ "$WHO" != "root" ]];then
+  echo 'USAGE: Please run as root'
+  exit 1
+fi  
+
+# Make sure we are on the right server
+hostnm=$(hostname)
+if [[ $hostnm =~ servera ]]; then
+  
+else
+  echo 'Usage: Needs to be run on servera as root'
+fi
