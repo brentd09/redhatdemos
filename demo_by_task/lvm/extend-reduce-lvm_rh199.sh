3e1609535287a7e92e14a254fb2ab1643d13f785
BrentD09 <brent.denny@lumifygroup.com>
2025-02-04 12:29:54 +1000

    Update and rename extend-lvm_rh199.sh to extend-reduce-lvm_rh199.sh

--- demo_by_task/lvm/extend-reduce-lvm_rh199.sh
+++ demo_by_task/lvm/extend-reduce-lvm_rh199.sh
@@ -10,6 +10,8 @@ fi
 hostnm=$(hostname)
 if [[ $hostnm =~ servera ]]; then
 
+  echo 'Add a partition and extend the VG'
+  echo ''
   echo 'parted /dev/vdc --script mkpart lvmextend etx4 601MB 3000MB'
   read -sp '' promptvar
   parted /dev/vdc --script mkpart lvmextend ext4 601MB 3000MB
@@ -43,6 +45,31 @@ if [[ $hostnm =~ servera ]]; then
   echo 'df'
   read -sp '' promptvar
   df
+  echo ''
+
+  #---------------------------
+  echo 'Remove /dev/vdd2 from the VG - first we must move data off the /dev/vdd2 extents'
+  echo 'and then we can reduce the size of the VG by removing the PV '
+  echo ''
+
+  echo 'lsblk'
+  read -sp '' promptvar
+  lsblk
+  echo ''
+
+  echo 'pvmove -A y /dev/vdd2'
+  read -sp '' promptvar
+  pvmove -A y /dev/vdd2
+  echo ''
+
+  echo 'vgreduce vg1_demo /dev/vdd2'
+  read -sp '' promptvar
+  vgreduce vg1_demo /dev/vdd2
+  echo ''
+
+  echo 'lsblk'
+  read -sp '' promptvar
+  lsblk
   echo ''
 
 else
