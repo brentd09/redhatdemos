a61b608847e35cf29ce38e223ec86347b76a635f
BrentD09 <brent.denny@lumifygroup.com>
2025-02-04 22:20:55 +1000

    Create revert-snapshot.sh

---
+++ rh-vmhost-scripts/revert-snapshot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+me=$(whoami)
+
+if [[ $me != root ]];then
+  echo "Please login as root to run this script"
+  exit 1
+fi
+
+if [[ $# -lt 1 ]];then
+  echo "Usage: $0 <kvm_vm_name>"
+  exit 2
+fi
+
+guests=$(virsh list --all --name | grep -v '^$' | sort | paste -sd ' ') 
+guests=${guests}" all"
+result=$(echo $guests | grep $1)
+
+if [[ $? -ne 0 ]];then
+  echo "The VM guests are : $guests"
+  echo "Usage: $0 <kvm_guest_name>"
+  exit 3
+fi
+
+if [[ "$1" == "all" ]];then
+  guestnames=$guests
+else 
+  guestnames=echo $guests | grep -o $1
+fi
+for vm_guest_name in $guestnames
+do
+  if [[ $vm_guest_name != all ]];then
+    echo "${vm_guest_name}: Reverting Snapshot"
+    virsh snapshot-revert --snapshotname ReadyForLabs $vm_guest_name
+  fi
+done
