533ef2b45a906c40a951b9c73a0aaadf015caa36
BrentD09 <brent.denny@lumifygroup.com>
2025-02-05 20:07:30 +1000

    Update basic-dnf-commands_rh199.sh

--- demo_by_task/dnf/basic-dnf-commands_rh199.sh
+++ demo_by_task/dnf/basic-dnf-commands_rh199.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
-
+echo "dnf list --installed"
+read -sp '' promptvar
 dnf list --installed
+echo ''
+
+echo "dnf list --available"
+read -sp '' promptvar
 dnf list --available
+echo ''
+
+echo "dnf info zip"
+read -sp '' promptvar
 dnf info zip
+echo ''
+
+echo "dnf provides zip"
+read -sp '' promptvar
 dnf provides zip
-# Search for a keyword 
+echo ''
+
+# Search for a keyword
+echo "dnf search zip"
+read -sp '' promptvar
 dnf search zip
+echo ''
+
+echo "dnf update"
+read -sp '' promptvar
 dnf update
+echo ''
+
 # List the files installed with from this rpm
+echo "dnf repoquery zip --list"
+read -sp '' promptvar
 dnf repoquery zip --list
+echo ''
+
+echo "dnf repoquery zip --location"
+read -sp '' promptvar
 dnf repoquery zip --location
+echo ''
