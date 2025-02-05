bf4832eb49f4ccaed0bdcf5157cced199a697b28
BrentD09 <brent.denny@lumifygroup.com>
2025-02-06 07:20:27 +1000

    Update dnf-modules_rh199.sh

--- demo_by_task/dnf/dnf-modules_rh199.sh
+++ demo_by_task/dnf/dnf-modules_rh199.sh
@@ -1,3 +1,14 @@
+echo "dnf module list"
+read -sp '' promptvar
 dnf module list
+echo ''
+
+echo "dnf module list nodejs"
+read -sp '' promptvar
 dnf module list nodejs
+echo ''
+
+echo "dnf module list provides nodejs"
+read -sp '' promptvar
 dnf module list provides nodejs
+echo ''
