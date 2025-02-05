4397a2e7334a4a694e18cea3b489b69dfb3f7a71
BrentD09 <brent.denny@lumifygroup.com>
2025-02-05 20:30:06 +1000

    Update dnf-history_rh199.sh

--- demo_by_task/dnf/dnf-history_rh199.sh
+++ demo_by_task/dnf/dnf-history_rh199.sh
@@ -1,6 +1,29 @@
+echo "dnf install httpd"
+read -sp '' promptvar
 dnf install httpd
+echo ''
+
+echo "grep SUBDEBUG /var/log/def.rpm.log"
+read -sp '' promptvar
 grep SUBDEBUG /var/log/def.rpm.log
+echo ''
+
+echo "dnf history"
+read -sp '' promptvar
 dnf history
+echo ''
+
+echo "dnf history | grep 'install httpd'"
+read -sp '' promptvar
 dnf history | grep 'install httpd'
+echo ''
+
+echo "history_num=$(dnf history | grep 'install httpd' | awk '{print $1}')"
+read -sp '' promptvar
 history_num=$(dnf history | grep 'install httpd' | awk '{print $1}')
+echo ''
+
+echo "dnf history undo $history_num"
+read -sp '' promptvar
 dnf history undo $history_num
+echo ''
