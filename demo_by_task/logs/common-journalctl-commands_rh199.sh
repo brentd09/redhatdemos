efbbae98f70cf5fab92e0f080ef337475d684c8e
BrentD09 <brent.denny@lumifygroup.com>
2025-02-06 09:04:02 +1000

    Update common-journalctl-commands_rh199.sh

--- demo_by_task/logs/common-journalctl-commands_rh199.sh
+++ demo_by_task/logs/common-journalctl-commands_rh199.sh
@@ -1,33 +1,59 @@
 # Using journalctl on a Red Hat Enterprise Linux (RHEL) 9 system can be very beneficial for system administrators and users for various reasons. Here are some real-world examples of scenarios where journalctl is helpful:
-
 # Troubleshooting Service Failures:
 # If a service fails to start or crashes
-journalctl -u <service_name> 
+echo "journalctl -u <service_name>"
+read -sp '' promptvar
+journalctl -u <service_name>
+echo ''
 
 # Analyzing System Boot Issues:
 # After a system reboot, if you suspect there were issues during boot
-journalctl -b 
+echo "journalctl -b"
+read -sp '' promptvar
+journalctl -b
+echo ''
 
 # Monitoring (Following) Logs in Real-time:
+echo "journalctl -f"
+read -sp '' promptvar
 journalctl -f
+echo ''
 
 # Reviewing User Login Activities:
 # To track user login attempts
+echo "journalctl _UID=<user_id>"
+read -sp '' promptvar
 journalctl _UID=<user_id>
+echo ''
 
 # Identifying Network Issues:
+echo "journalctl -t NetworkManager"
+read -sp '' promptvar
 journalctl -t NetworkManager
+echo ''
 
 # Tracking Security Events:
+echo "journalctl -p auth"
+read -sp '' promptvar
 journalctl -p auth
+echo ''
 
 # Filtering by Time Period:
+echo "journalctl --since "2025-05-01 12:00:00" --until "2025-05-01 14:00:00""
+read -sp '' promptvar
 journalctl --since "2025-05-01 12:00:00" --until "2025-05-01 14:00:00"
+echo ''
 
 # Debugging Application Issues:
 # If a custom application generates log information
+echo "journalctl -t my-application-tag"
+read -sp '' promptvar
 journalctl -t my-application-tag
+echo ''
 
 # Checking for Kernel Messages:
 # Helpful in diagnosing hardware failures or kernel-related problems.
+echo "journalctl -k"
+read -sp '' promptvar
 journalctl -k
+echo ''
