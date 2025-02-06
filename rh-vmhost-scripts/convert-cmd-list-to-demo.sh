4ced1c1f6e9ad28634d267815863d31dde56855e
BrentD09 <brent.denny@lumifygroup.com>
2025-02-06 10:17:53 +1000

    Update convert-cmd-list-to-demo.sh

--- rh-vmhost-scripts/convert-cmd-list-to-demo.sh
+++ rh-vmhost-scripts/convert-cmd-list-to-demo.sh
@@ -6,6 +6,10 @@ if [[ -f $1 ]];then
       echo $line
       continue
     fi    
+    if [[ $line =~ ^echo ]];then 
+      echo $line
+      continue
+    fi 
     echo "echo \"$line\""
     echo "read -sp '' promptvar"
     echo $line
