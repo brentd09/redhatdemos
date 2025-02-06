81958ba4c33a9e2ef4789d7771b78f6e9bcfc426
BrentD09 <brent.denny@lumifygroup.com>
2025-02-06 10:28:34 +1000

    Update preserve-journal_rh199.sh

--- demo_by_task/logs/preserve-journal_rh199.sh
+++ demo_by_task/logs/preserve-journal_rh199.sh
@@ -1,5 +1,20 @@
+echo "mkdir -p /var/log/journal"
+read -sp '' promptvar
 mkdir -p /var/log/journal
+echo ''
+
 echo Edit the /etc/systemd/journald.conf file to set the log to be Persistent
-sed -i "/s/Storage=.\+/Storage=persistent/g" /etc/systemd/journald.conf
+echo "sed -i "/s/Storage=.+/Storage=persistent/g" /etc/systemd/journald.conf"
+read -sp '' promptvar
+sed -i "/s/Storage=.+/Storage=persistent/g" /etc/systemd/journald.conf
+echo ''
+
+echo "less /etc/systemd/journald.conf"
+read -sp '' promptvar
 less /etc/systemd/journald.conf
+echo ''
+
+echo "systemctl restart systemd-journald"
+read -sp '' promptvar
 systemctl restart systemd-journald
+echo ''
