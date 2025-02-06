7fc1425f0f144c3de59ceda542059218e19aec7b
BrentD09 <brent.denny@lumifygroup.com>
2025-02-06 13:17:16 +1000

    Update manage-time_rh199.sh

--- demo_by_task/logs-time/manage-time_rh199.sh
+++ demo_by_task/logs-time/manage-time_rh199.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
+echo "timedatectl"
+read -sp '' promptvar
+timedatectl
+echo ''
 
-timedatectl
+echo "timedatectl --list-timezones"
+read -sp '' promptvar
 timedatectl --list-timezones
+echo ''
+
+echo "timedatectl --list-timezones | grep -i australia"
+read -sp '' promptvar
 timedatectl --list-timezones | grep -i australia
+echo ''
+
+echo "timedatectl set-timezone Australia/Brisbane"
+read -sp '' promptvar
 timedatectl set-timezone Australia/Brisbane
+echo ''
+
+echo "timedatectl"
+read -sp '' promptvar
 timedatectl
+echo ''
+
+echo "timedatectl set-ntp true"
+read -sp '' promptvar
 timedatectl set-ntp true
+echo ''
 
 # chronyd time sync service
+echo "vim /etc/chrony.conf"
+read -sp '' promptvar
 vim /etc/chrony.conf
+echo ''
+
+echo "systemctl restart chronyd.service"
+read -sp '' promptvar
 systemctl restart chronyd.service
+echo ''
+
 # force an ntp timesync
+echo "chronyc makestep"
+read -sp '' promptvar
 chronyc makestep
+echo ''
+
+echo "chronyc sources -v"
+read -sp '' promptvar
 chronyc sources -v
+echo ''
