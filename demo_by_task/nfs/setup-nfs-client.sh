f00cf3bd1f69601eecaa229496640ca68e919c96
BrentD09 <brent.denny@lumifygroup.com>
2025-02-06 14:49:18 +1000

    Create setup-nfs-client.sh

---
+++ demo_by_task/nfs/setup-nfs-client.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+me=$(whoami)
+if [[ $me != root ]];then
+  echo 'Usage: Must be run as root on serverb'
+fi
+
+this_machine=$(hostname)
+if [[ $this_machine != servera ]];then
+  echo 'Usage: Must be run as root on serverb'
+  exit1
+fi
+
+echo "dnf install -y nfs-utils"
+read -sp '' promptvar
+dnf install -y nfs-utils
+echo ''
+
+echo "mkdir -p /mnt/serverb_nfs"
+read -sp '' promptvar
+mkdir -p /mnt/serverbnfs
+echo ''
+
+echo "mount -t nfs serverb:/srv/nfs_share /mnt/serverb_nfs"
+read -sp '' promptvar
+mount -t nfs serverb:/srv/nfs_share /mnt/serverb_nfs
+echo ''
+
+
+echo "touch /mnt/serverb_nfs/newfile"
+read -sp '' promptvar
+touch /mnt/serverb_nfs/newfile
+echo ''
