1ce3ee7aefbb83d63ee27b51a0907bab4f50da54
BrentD09 <brent.denny@lumifygroup.com>
2025-02-06 16:22:53 +1000

    Update setup-nfs-client.sh

--- demo_by_task/nfs/setup-nfs-client.sh
+++ demo_by_task/nfs/setup-nfs-client.sh
@@ -26,8 +26,25 @@ read -sp '' promptvar
 mount -t nfs serverb:/srv/nfs_share /mnt/serverb_nfs
 echo ''
 
-
 echo "touch /mnt/serverb_nfs/newfile"
 read -sp '' promptvar
 touch /mnt/serverb_nfs/newfile
+echo ''
+
+echo to make the mount persistent add it to the /etc/fstab file
+echo "vim /etc/fstab"
+read -sp '' promptvar
+echo "serverb:/srv/nfs_share  /mnt/serverb_nfs nfs rw 0 0" >> /etc/fstab
+vi /etc/fstab
+echo ''
+
+echo 'When unmounting the NFS mount point, first check to see if anyone is using the NFS share'
+echo "lsof /mnt/serverb_nfs/"
+read -sp '' promptvar
+lsof /mnt/serverb_nfs/
+echo ''
+
+echo "umount /mnt/serverb_nfs"
+read -sp '' promptvar
+umount /mnt/serverb_nfs
 echo ''
