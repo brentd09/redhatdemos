ed9c4b45e91d2e02781ea396557990b08ff7344f
BrentD09 <brent.denny@lumifygroup.com>
2025-02-06 14:40:46 +1000

    Create build-nfs-server_rh199.sh

---
+++ demo_by_task/nfs/build-nfs-server_rh199.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#this_machine=$(hostname)
+#if [[ $this_machine != serverb ]];then
+#  echo 'Usage: This needs to be run on serverb'
+#  exit1
+#fi
+
+dnf install -y nfs-utils
+systemctl enable --now nfs-server rpcbind nfs-mountd
+systemctl status nfs-server
+firewall-cmd --permanent --add-service=nfs
+firewall-cmd --permanent --add-service=rpc-bind 
+firewall-cmd --permanent --add-service=mountd 
+firewall-cmd --reload 
+firewall-cmd --list-all
+   
+mkdir -p /srv/nfs_share
+chown -R nobody:nobody /srv/nfsr_hare
+chmod 777 /srv/nfsr_hare
+echo '/srv/nfs_share  192.168.122.0/24(rw,sync,no_root_squash)' >> /etc/exports
+# Refresh exports
+exportfs -r
+systemctl restart nfs-server.service 
+# Show exports verbose option
+exportfs -v
+tree /srv/
