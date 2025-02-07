5ca4336f460e6e8775e26e4d4d2fb7b7c0c755ef
BrentD09 <brent.denny@lumifygroup.com>
2025-02-08 00:16:19 +1000

    Update setup-nfs-client.sh

--- demo_by_task/nfs/setup-nfs-client.sh
+++ demo_by_task/nfs/setup-nfs-client.sh
@@ -11,48 +11,56 @@ if [[ $this_machine != servera ]];then
   exit1
 fi
 
-echo "dnf install -y nfs-utils"
+echo -n "dnf install -y nfs-utils"
 read -sp '' promptvar
+echo ''
 dnf install -y nfs-utils
 echo ''
 
-echo "mkdir -p /mnt/serverb_nfs"
+echo -n "mkdir -p /mnt/serverb_nfs"
 read -sp '' promptvar
+echo ''
 mkdir -p /mnt/serverb_nfs
 echo ''
 
-echo "mount -t nfs serverb:/srv/nfs_share /mnt/serverb_nfs"
+echo -n "mount -t nfs serverb:/srv/nfs_share /mnt/serverb_nfs"
 read -sp '' promptvar
+echo ''
 mount -t nfs serverb:/srv/nfs_share /mnt/serverb_nfs
 echo ''
 
-echo "touch /mnt/serverb_nfs/newfile"
+echo -n "touch /mnt/serverb_nfs/newfile"
 read -sp '' promptvar
+echo ''
 touch /mnt/serverb_nfs/newfile
 echo ''
 
 echo to make the mount persistent add it to the /etc/fstab file
-echo "vim /etc/fstab"
+echo -n "vim /etc/fstab"
 read -sp '' promptvar
 echo "serverb:/srv/nfs_share  /mnt/serverb_nfs nfs rw 0 0" >> /etc/fstab
+echo ''
 vi /etc/fstab
 echo ''
 
 echo 'When unmounting the NFS mount point, first check to see if anyone is using the NFS share'
-echo "cd /mnt/serverb_nfs/"
+echo -n "cd /mnt/serverb_nfs/"
 cd /mnt/serverb_nfs/
-echo "lsof /mnt/serverb_nfs/"
+echo -n "lsof /mnt/serverb_nfs/"
 read -sp '' promptvar
+echo ''
 lsof /mnt/serverb_nfs/ 2> /dev/null
 echo ''
-echo "cd "
+echo -n "cd "
 cd
-echo "lsof /mnt/serverb_nfs/"
+echo -n "lsof /mnt/serverb_nfs/"
 read -sp '' promptvar
+echo ''
 lsof /mnt/serverb_nfs/ 2> /dev/null
 echo ''
 
-echo "umount /mnt/serverb_nfs"
+echo -n "umount /mnt/serverb_nfs"
 read -sp '' promptvar
+echo ''
 umount /mnt/serverb_nfs
 echo ''
