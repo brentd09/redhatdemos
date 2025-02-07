69a6999609c953c5ec6e372a90bab6caab784528
BrentD09 <brent.denny@lumifygroup.com>
2025-02-07 17:20:00 +1000

    Update swap-partition_rh199.sh

--- demo_by_task/disk/swap-partition_rh199.sh
+++ demo_by_task/disk/swap-partition_rh199.sh
@@ -10,8 +10,9 @@ fi
 hostnm=$(hostname)
 if [[ $hostnm =~ servera ]]; then
   echo 'Define the disk and assign it the variable DISK  (e.g., /dev/sda)'
-  echo 'DISK="/dev/vdb"'
+  echo -n 'DISK="/dev/vdb"'
   read -sp '' promptvar
+  echo ''
   DISK="/dev/vdb"
   
   # Automate partitioning of using parted
@@ -19,44 +20,51 @@ if [[ $hostnm =~ servera ]]; then
   diskparttype=$(parted -sm $DISK print)
   if [[ $diskparttype =~ 'unknown' ]];then 
     echo 'Create a GPT partition table'
-    echo 'parted $DISK --script mklabel gpt'
+    echo -n 'parted $DISK --script mklabel gpt'
     read -sp '' promptvar
     parted $DISK --script mklabel gpt
+    echo ''
   fi
   
   echo Create the first swap partition of 500 MB
-  echo 'parted $DISK --script mkpart swap1 linux-swap 2001MB 2500MB'
+  echo -n 'parted $DISK --script mkpart swap1 linux-swap 2001MB 2500MB'
   read -sp '' promptvar
   parted $DISK --script mkpart swap1 linux-swap 2001MB 2500MB
+  echo ''
   
   echo 'Create the second partition'
-  echo 'parted $DISK --script mkpart swap2 linux-swap 2501MB 3000MB'
+  echo -n 'parted $DISK --script mkpart swap2 linux-swap 2501MB 3000MB'
   read -sp '' promptvar
   parted $DISK --script mkpart swap2 linux-swap 2501MB 3000MB
+  echo ''
   
   echo 'Print the partition table to verify'
-  echo 'parted $DISK --script print'
+  echo -n 'parted $DISK --script print'
   parted $DISK --script print
   echo ''
 
   echo 'Wait for device actions to complete before proceeding'
-  echo 'udevadm settle'
+  echo -n 'udevadm settle'
   read -sp '' promptvar
   udevadm settle
-
+  echo ''
+  
+  
   echo Locating the swap partition device names then format and activate them
   echo 'devnames=$(lsblk -o KNAME,PARTTYPENAME | grep swap | awk {print "/dev/"$1})'
   echo 'for device in $devnames;do'
   echo '  mkswap $device'
   echo '  swapon $device'
-  echo 'done'
+  echo -n 'done'
   read -sp '' promptvar
+  echo ''
   devnames=$(lsblk -o PATH,PARTTYPENAME | grep swap | awk '{print $1}')
   for device in $devnames;do
     mkswap $device &> /dev/null
     swapon $device &> /dev/null
   done
   echo ''
+  
   echo 'Making the swap partitions persistent'
   echo 'Adding the swap partitions to the /etc/fstab file'
   cp /etc/fstab /etc/fstab.backup
