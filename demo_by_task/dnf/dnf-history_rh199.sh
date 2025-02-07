39e50f5767002883a41ce61ad0e7803d789b4fbe
BrentD09 <brent.denny@lumifygroup.com>
2025-02-07 17:22:41 +1000

    Update dnf-history_rh199.sh

--- demo_by_task/dnf/dnf-history_rh199.sh
+++ demo_by_task/dnf/dnf-history_rh199.sh
@@ -1,29 +1,35 @@
-echo "dnf install httpd"
+echo -n "dnf install httpd"
 read -sp '' promptvar
+echo ''
 dnf install httpd
 echo ''
 
-echo "grep SUBDEBUG /var/log/def.rpm.log"
+echo -n "grep SUBDEBUG /var/log/def.rpm.log"
 read -sp '' promptvar
+echo ''
 grep SUBDEBUG /var/log/def.rpm.log
 echo ''
 
-echo "dnf history"
+echo -n "dnf history"
 read -sp '' promptvar
+echo ''
 dnf history
 echo ''
 
-echo "dnf history | grep 'install httpd'"
+echo -n "dnf history | grep 'install httpd'"
 read -sp '' promptvar
+echo ''
 dnf history | grep 'install httpd'
 echo ''
 
-echo "history_num=$(dnf history | grep 'install httpd' | awk '{print $1}')"
+echo -n "history_num=$(dnf history | grep 'install httpd' | awk '{print $1}')"
 read -sp '' promptvar
+echo ''
 history_num=$(dnf history | grep 'install httpd' | awk '{print $1}')
 echo ''
 
-echo "dnf history undo $history_num"
+echo -n "dnf history undo $history_num"
 read -sp '' promptvar
+echo ''
 dnf history undo $history_num
 echo ''
