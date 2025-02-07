141e7b9efb13080f49c9c42f80e5f80e9a6086c7
BrentD09 <brent.denny@lumifygroup.com>
2025-02-07 17:25:33 +1000

    Update common-journalctl-commands_rh199.sh

--- demo_by_task/logs-time/common-journalctl-commands_rh199.sh
+++ demo_by_task/logs-time/common-journalctl-commands_rh199.sh
@@ -1,34 +1,38 @@
 # Using journalctl on a Red Hat Enterprise Linux (RHEL) 9 system can be very beneficial for system administrators and users for various reasons. Here are some real-world examples of scenarios where journalctl is helpful:
 echo Troubleshooting Service Failures:
 echo If a service fails to start or crashes
-echo "journalctl -u sshd.service"
+echo -n "journalctl -u sshd.service"
 read -sp '' promptvar
+echo ''
 journalctl -u sshd.service
 echo ''
 
 # Analyzing System Boot Issues:
 echo After a system reboot, if you suspect there were issues during boot
-echo "journalctl -b"
+echo -n "journalctl -b"
 read -sp '' promptvar
+echo ''
 journalctl -b
 echo ''
 
 echo Reviewing User Activities:
-echo "journalctl _UID=1000"
+echo -n "journalctl _UID=1000"
 read -sp '' promptvar
 journalctl _UID=1000
 echo ''
 
 echo Identifying Network Issues:
-echo "journalctl -t NetworkManager"
+echo -n "journalctl -t NetworkManager"
 read -sp '' promptvar
+echo ''
 journalctl -t NetworkManager
 echo ''
 
 echo Tracking Events by Severity:
 echo Options are emerg,alert,crit,err,warning,notice,info,debug
-echo "journalctl -p err"
+echo -n "journalctl -p err"
 read -sp '' promptvar
+echo ''
 journalctl -p err
 echo ''
 
@@ -36,27 +40,31 @@ curr_date=$(date "+%Y-%m-%d")
 yesterday=$(date -d "1 day ago" "+%Y-%m-%d")
 
 echo Filtering by Time Period:
-echo "journalctl --since $yesterday --until $curr_date "
+echo -n "journalctl --since $yesterday --until $curr_date "
 read -sp '' promptvar
+echo ''
 journalctl --since  $yesterday  --until  $curr_date 
 echo ''
 
 echo Debugging Application Issues:
 # If a custom application generates log information
-echo "journalctl -t sshd"
+echo -n "journalctl -t sshd"
 read -sp '' promptvar
+echo ''
 journalctl -t sshd
 echo ''
 
 echo Checking for Kernel Messages:
 # Helpful in diagnosing hardware failures or kernel-related problems.
-echo "journalctl -k"
+echo -n "journalctl -k"
 read -sp '' promptvar
+echo ''
 journalctl -k
 echo ''
 
 echo 'Monitoring (Following) Logs in Real-time:'
-echo "journalctl -f"
+echo -n "journalctl -f"
 read -sp '' promptvar
+echo ''
 journalctl -f
 echo ''
