33d6b3279755ec8591aaddf368d1b5e81ee6ec0f
BrentD09 <brent.denny@lumifygroup.com>
2025-02-08 00:03:24 +1000

    Update autofs-client-indirect-vs-direct-setup_rh199.sh

--- demo_by_task/nfs/autofs-client-indirect-vs-direct-setup_rh199.sh
+++ demo_by_task/nfs/autofs-client-indirect-vs-direct-setup_rh199.sh
@@ -12,23 +12,26 @@ if [[ $this_machine != servera ]];then
   exit2
 fi
 
-echo "dnf install -y nfs-utils"
+echo -n  "dnf install -y nfs-utils"
 read -sp '' promptvar
+echo ''
 dnf install -y nfs-utils
 echo ''
 
-echo "dnf install -y autofs"
+echo -n "dnf install -y autofs"
 read -sp '' promptvar
+echo ''
 dnf install -y autofs
 echo ''
 
 
-echo 'Edit the /etc/auto.master file to point to the indirect mount file'
+echo -n 'Edit the /etc/auto.master file to point to the indirect mount file'
 read -sp '' promptvar
+echo ''
 echo '/projects /etc/auto.projects --timeout=60' > /etc/auto.master
 vi /etc/auto.master
 
-echo 'Edit the /etc/auto.projects file to include the indirect mounts'
+echo -n 'Edit the /etc/auto.projects file to include the indirect mounts'
 read -sp '' promptvar
 echo 'project1 -fstype=nfs,rw,nosuid,nodev serverb:/srv/project1' > /etc/auto.projects
 echo 'project2 -fstype=nfs,rw,nosuid,nodev serverb:/srv/project2' >> /etc/auto.projects
@@ -36,53 +39,63 @@ echo 'project3 -fstype=nfs,rw,nosuid,nodev serverb:/srv/project3' >> /etc/auto.p
 echo '# Because these three exports are sub-directories of /srv on serverb '  >> /etc/auto.projects
 echo '# the three lines could be replace with the following '  >> /etc/auto.projects
 echo '# * -fstype=nfs,rw,nosuid,nodev serverb:/srv/&'  >> /etc/auto.projects
+echo ''
 vi /etc/auto.projects
 
-echo "mkdir /projects"
+echo -n "mkdir /projects"
 read -sp '' promptvar
+echo ''
 mkdir /projects
 echo ''
 
-echo "chmod 755 /projects/"
+echo -n "chmod 755 /projects/"
 read -sp '' promptvar
+echo ''
 chmod 755 /projects/
 echo ''
 
-echo "systemctl restart autofs"
+echo -n "systemctl restart autofs"
 read -sp '' promptvar
+echo ''
 systemctl restart autofs
 echo ''
 
-echo "cd /projects"
+echo -n "cd /projects"
 read -sp '' promptvar
+echo ''
 cd /projects
 echo ''
 
-echo "ls"
+echo -n "ls"
 read -sp '' promptvar
+echo ''
 ls
 echo ''
 
-echo "ls project1"
+echo -n "ls project1"
 read -sp '' promptvar
+echo ''
 ls project1
 echo ''
 
-echo "ls project2"
+echo -n "ls project2"
 read -sp '' promptvar
+echo ''
 ls project2
 echo ''
 
-echo "tree /projects"
+echo -n "tree /projects"
 read -sp '' promptvar
+echo ''
 tree /projects
 echo ''
 
 echo '------------- Direct Automount -------------------------'
 
-echo '/- /etc/auto.project.direct' >> /etc/auto.master
-echo "vi /etc/auto.master"
+echo  '/- /etc/auto.project.direct' >> /etc/auto.master
+echo -n "vi /etc/auto.master"
 read -sp '' promptvar
+echo ''
 vi /etc/auto.master
 echo ''
 
@@ -90,43 +103,51 @@ echo '/directprojects/proj1 -fstype=nfs,rw,nosuid,nodev serverb:/srv/project1' >
 echo '/directprojects/proj2 -fstype=nfs,rw,nosuid,nodev serverb:/srv/project2' >> /etc/auto.project.direct
 echo '/directprojects/proj3 -fstype=nfs,rw,nosuid,nodev serverb:/srv/project3' >> /etc/auto.project.direct
 
-echo "vi /etc/auto.project.direct"
+echo -n "vi /etc/auto.project.direct"
 read -sp '' promptvar
+echo ''
 vi /etc/auto.project.direct
 echo ''
 
-echo "mkdir -p /directprojects/"
+echo -n "mkdir -p /directprojects/"
 read -sp '' promptvar
+echo ''
 mkdir -p /directprojects/
 echo ''
 
-echo "systemctl restart autofs"
+echo -n "systemctl restart autofs"
 read -sp '' promptvar
+echo ''
 systemctl restart autofs
 echo ''
 
-echo "cd /directprojects/"
+echo -n "cd /directprojects/"
 read -sp '' promptvar
+echo ''
 cd /directprojects/
 echo ''
 
-echo "ls"
+echo -n "ls"
 read -sp '' promptvar
+echo ''
 ls
 echo ''
 
-echo "cd proj1"
+echo -n "cd proj1"
 read -sp '' promptvar
+echo ''
 cd proj1
 echo ''
 
-echo "ls"
+echo -n "ls"
 read -sp '' promptvar
+echo ''
 ls
 echo ''
 
-echo "touch file1"
+echo -n "touch file1"
 read -sp '' promptvar
+echo ''
 touch file1
 echo ''
 
