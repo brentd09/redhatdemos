67d5135415ee401141a21cbff7508ef855712c1b
BrentD09 <brent.denny@lumifygroup.com>
2025-02-07 17:31:06 +1000

    Update extend-reduce-lvm_rh199.sh

--- demo_by_task/lvm/extend-reduce-lvm_rh199.sh
+++ demo_by_task/lvm/extend-reduce-lvm_rh199.sh
@@ -14,46 +14,54 @@ if [[ $hostnm =~ servera ]]; then
   echo 'If we tried to add or exten an LV we would need the' 
   echo 'extents available in the VG which we do not have'
   echo ''
-  echo 'lvcreate --name lv3_demo -L 3GB vg1_demo'
+  echo -n 'lvcreate --name lv3_demo -L 3GB vg1_demo'
   read -sp '' promptvar
+  echo ''
   lvcreate --name lv3_demo -L 3GB vg1_demo
   echo ''
 
 
   echo 'Add a partition and extend the VG'
   echo ''
-  echo 'parted /dev/vdc --script mkpart lvmextend etx4 601MB 3000MB'
+  echo -n 'parted /dev/vdc --script mkpart lvmextend etx4 601MB 3000MB'
   read -sp '' promptvar
+  echo ''
   parted /dev/vdc --script mkpart lvmextend ext4 601MB 3000MB
   echo ''
 
-  echo 'lsblk'
+  echo -n 'lsblk'
   read -sp '' promptvar
+  echo ''
   lsblk
   echo ''  
 
-  echo 'pvcreate /dev/vdc4'
+  echo -n 'pvcreate /dev/vdc4'
   read -sp '' promptvar
+  echo ''
   pvcreate /dev/vdc4
   echo ''
 
-  echo 'vgextend vg1_demo /dev/vdc4'
+  echo -n 'vgextend vg1_demo /dev/vdc4'
   read -sp '' promptvar
+  echo ''
   vgextend vg1_demo /dev/vdc4
   echo ''
 
-  echo 'lvextend -L +2GB /dev/vg1_demo/lv2_demo'
+  echo -n 'lvextend -L +2GB /dev/vg1_demo/lv2_demo'
   read -sp '' promptvar
+  echo ''
   lvextend -L +2GB /dev/vg1_demo/lv2_demo
   echo ''
 
-  echo 'resize2fs /dev/vg1_demo/lv2_demo'
+  echo -n 'resize2fs /dev/vg1_demo/lv2_demo'
   read -sp '' promptvar
+  echo ''
   resize2fs /dev/vg1_demo/lv2_demo
   echo ''
 
-  echo 'df'
+  echo -n 'df'
   read -sp '' promptvar
+  echo ''
   df
   echo ''
 
@@ -62,23 +70,27 @@ if [[ $hostnm =~ servera ]]; then
   echo 'and then we can reduce the size of the VG by removing the PV '
   echo ''
 
-  echo 'lsblk'
+  echo -n 'lsblk'
   read -sp '' promptvar
+  echo ''
   lsblk
   echo ''
 
-  echo 'pvmove -A y /dev/vdd2'
+  echo -n 'pvmove -A y /dev/vdd2'
   read -sp '' promptvar
+  echo ''
   pvmove -A y /dev/vdd2
   echo ''
 
-  echo 'vgreduce vg1_demo /dev/vdd2'
+  echo -n 'vgreduce vg1_demo /dev/vdd2'
   read -sp '' promptvar
+  echo ''
   vgreduce vg1_demo /dev/vdd2
   echo ''
 
-  echo 'lsblk'
+  echo -n 'lsblk'
   read -sp '' promptvar
+  echo ''
   lsblk
   echo ''
 
