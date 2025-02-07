d368132d8e22cbc36242d2f42723f29093c931d0
BrentD09 <brent.denny@lumifygroup.com>
2025-02-07 17:16:03 +1000

    Update mount-filesystems_rh199.sh

--- demo_by_task/disk/mount-filesystems_rh199.sh
+++ demo_by_task/disk/mount-filesystems_rh199.sh
@@ -22,58 +22,70 @@ if [[ $part1result -ne 0 ||  $part2result -ne 0 ]]; then
   exit 1
 fi
 
-echo 'mkdir -p /mnt/salesdata'
+echo -n 'mkdir -p /mnt/salesdata'
 read -sp '' promptvar
+echo ''
 mkdir -p /mnt/salesdata
 echo ''
 
-echo 'mkdir -p /mnt/hrdata'
+echo -n 'mkdir -p /mnt/hrdata'
 read -sp '' promptvar
+echo ''
 mkdir -p /mnt/hrdata
 echo ''
 
-echo 'lsblk -fp '
+echo -n 'lsblk -fp '
 read -sp '' promptvar
+echo ''
 lsblk -fp 
 echo ''
 
 vdc1guid=$(lsblk -fp /dev/vdc1 | grep -oP '[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}')
 
-echo "mount UUID=$vdc1guid /mnt/salesdata"
+echo -n "mount UUID=$vdc1guid /mnt/salesdata"
 read -sp '' promptvar
+echo ''
 mount UUID=$vdc1guid /mnt/salesdata
 echo ''
 
-echo 'mount /dev/vdc2 /mnt/hrdata'
+echo -n 'mount /dev/vdc2 /mnt/hrdata'
 read -sp '' promptvar
+echo ''
 mount /dev/vdc2 /mnt/hrdata
 echo ''
 
-echo 'df'
+echo -n 'df'
 read -sp '' promptvar
+echo ''
 df
 echo ''
 
-echo 'touch /mnt/salesdata/sales.db'
+echo -n 'touch /mnt/salesdata/sales.db'
 read -sp '' promptvar
+echo ''
 touch /mnt/salesdata/sales.db
 echo ''
-echo 'touch /mnt/hrdata/hr.db'
+
+echo -n 'touch /mnt/hrdata/hr.db'
 read -sp '' promptvar
+echo ''
 touch /mnt/hrdata/hr.db
 echo ''
 
-echo 'ls /mnt/salesdata/ /mnt/hrdata/'
+echo -n 'ls /mnt/salesdata/ /mnt/hrdata/'
 read -sp '' promptvar
+echo ''
 ls /mnt/salesdata/ /mnt/hrdata/
 echo ''
 
-echo 'umount /mnt/salesdata/ /mnt/hrdata/'
+echo -n 'umount /mnt/salesdata/ /mnt/hrdata/'
 read -sp '' promptvar
+echo ''
 umount /mnt/salesdata/ /mnt/hrdata/
 echo ''
 
-echo 'ls /mnt/salesdata/ /mnt/hrdata/'
+echo -n 'ls /mnt/salesdata/ /mnt/hrdata/'
 read -sp '' promptvar
+echo ''
 ls /mnt/salesdata/ /mnt/hrdata/
 echo ''
