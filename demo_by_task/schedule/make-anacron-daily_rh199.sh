c56bfa6c210ca064e22232592ebbf470c7f5a868
BrentD09 <brent.denny@lumifygroup.com>
2025-02-09 22:52:43 +1000

    Update make-anacron-daily_rh199.sh

--- demo_by_task/schedule/make-anacron-daily_rh199.sh
+++ demo_by_task/schedule/make-anacron-daily_rh199.sh
@@ -4,12 +4,14 @@ cat /etc/anacrontab
 echo ''
 
 echo Create a script in /etc/cron.daily/log-datetime.sh
-sudo echo '#!/bin/bash' > /etc/cron.daily/log-datetime.sh
-sudo echo '' >> /etc/cron.daily/log-datetime.sh
-sudo echo 'for a in 1 2 3 4 5; do' >> /etc/cron.daily/log-datetime.sh
-sudo echo '  date >> /usr/tmp/my_date.log' >> /etc/cron.daily/log-datetime.sh
-sudo echo '  sleep 2' >> /etc/cron.daily/log-datetime.sh
-sudo echo 'done' >> /etc/cron.daily/log-datetime.sh
+echo '#!/bin/bash' > /etc/cron.daily/log-datetime.sh
+echo '' >> /etc/cron.daily/log-datetime.sh
+echo 'for a in 1 2 3 4 5; do' >> /etc/cron.daily/log-datetime.sh
+echo '  date >> /usr/tmp/my_date.log' >> /etc/cron.daily/log-datetime.sh
+echo '  sleep 2' >> /etc/cron.daily/log-datetime.sh
+echo 'done' >> /etc/cron.daily/log-datetime.sh
+
+
 
 chmod 777 /etc/cron.daily/log-datetime.sh
 
