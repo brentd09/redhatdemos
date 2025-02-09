480624ecab1c5e42ca23ad468a099a8694408427
BrentD09 <brent.denny@lumifygroup.com>
2025-02-09 22:32:54 +1000

    Update create-container_rh134.sh

--- demo_by_task/podman/create-container_rh134.sh
+++ demo_by_task/podman/create-container_rh134.sh
@@ -1,5 +1,13 @@
-ssh student@servera
-sudo -i
+#!/bin/bash
+
+#me=$(whoami)
+
+
+#if [[ $me != root ]];then
+#  echo 'Usage: Must be run as root on servera'
+#  exit 1
+#fi
+
 
 dnf install -y podman cockpit-podman cockpit
 systemctl enable --now podman
@@ -15,12 +23,12 @@ podman ps
 podman stop ubi-demo-container
 
  
-cat <<EOF  > Containerfile
-FROM registry.access.redhat.com/ubi9/ubi
-RUN dnf install -y httpd && echo "PODMAN demo website from container" > /var/www/html/index.html
-EXPOSE 80
-CMD ["httpd", "-D", "FOREGROUND"]
-EOF
+#cat <<EOF  > Containerfile
+#FROM registry.access.redhat.com/ubi9/ubi
+#RUN dnf install -y httpd && echo "PODMAN demo website from container" > /var/www/html/index.html
+#EXPOSE 80
+#CMD ["httpd", "-D", "FOREGROUND"]
+#EOF
 
 podman build -t demo_image_rh134 .
 podman run -d -p 8080:80 --name demo_container_rh134 localhost/demo_image_rh134:latest
