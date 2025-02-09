1fa0c38075652e317cfb00bc18f80e20e9032554
BrentD09 <brent.denny@lumifygroup.com>
2025-02-09 22:59:15 +1000

    Update commands_rh199.sh

--- demo_by_task/selinux/commands_rh199.sh
+++ demo_by_task/selinux/commands_rh199.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 echo Check selinux enforcement
-echo getenforce
+echo -n 'getenforce'
 read -sp '' promptvar
+echo ''
 getenforce
 echo ''
 
 echo Set selinux enforcement to permissive
-echo setenforce permissive
+echo -n 'setenforce permissive'
 read -sp '' promptvar
+echo ''
 sudo setenforce permissive
+echo ''
+
+echo Set selinux enforcement to permissive
+echo -n 'getenforce'
+read -sp '' promptvar
+echo ''
 getenforce
 echo ''
 
@@ -17,5 +25,11 @@ echo Set selinux enforcement to enforcing
 echo setenforce enforcing
 read -sp '' promptvar
 sudo setenforce enforcing
+echo ''
+
+echo Set selinux enforcement to permissive
+echo -n 'getenforce'
+read -sp '' promptvar
+echo ''
 getenforce
 echo ''
