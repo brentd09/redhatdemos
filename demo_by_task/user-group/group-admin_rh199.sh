6f41fa32b5ec806b8c2115c30bc67c183e4643a2
BrentD09 <brent.denny@lumifygroup.com>
2025-02-09 23:14:43 +1000

    Update group-admin_rh199.sh

--- demo_by_task/user-group/group-admin_rh199.sh
+++ demo_by_task/user-group/group-admin_rh199.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 echo Add new group
-echo groupadd demogroup1
+echo -n 'groupadd demogroup1'
 read -sp '' promptvar
-sudo groupadd demogroup1
+echo ''
+groupadd demogroup1
 echo ''
 
 # groupmod does not allow group membership changes just Group Nme and GID changes
 echo Change group name
-echo groupmod -n demogroup2 demogroup1
+echo -n 'groupmod -n demogroup2 demogroup1'
 read -sp '' promptvar
-sudo groupmod -n demogroup2 demogroup1
+echo ''
+groupmod -n demogroup2 demogroup1
 echo ''
 
 echo Change group GID
-echo groupmod -g 1100 demogroup2
+echo -n 'groupmod -g 1100 demogroup2'
 read -sp '' promptvar
-sudo groupmod -g 1100 demogroup2
+echo ''
+groupmod -g 1100 demogroup2
 echo ''
