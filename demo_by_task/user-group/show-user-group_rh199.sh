79afa9df294207c5a2f97884fcde0fbac728d812
BrentD09 <brent.denny@lumifygroup.com>
2025-02-09 23:18:52 +1000

    Update show-user-group_rh199.sh

--- demo_by_task/user-group/show-user-group_rh199.sh
+++ demo_by_task/user-group/show-user-group_rh199.sh
@@ -1,34 +1,58 @@
 
+echo -n "id"
+read -sp '' promptvar
+echo ''
 id
+echo ''
 
-
-
+echo -n "id devops"
+read -sp '' promptvar
+echo ''
 id devops
+echo ''
 
-
-
+echo -n "ls -ld ~/Downloads"
+read -sp '' promptvar
+echo ''
 ls -ld ~/Downloads
+echo ''
 
 
+echo -n "ps -ef"
+read -sp '' promptvar
+echo ''
+ps -ef
+echo ''
 
-ps -ef 
-
-
+echo -n "cat /etc/passwd"
+read -sp '' promptvar
+echo ''
 cat /etc/passwd
+echo ''
 
 
-
+echo -n "cat /etc/group"
+read -sp '' promptvar
+echo ''
 cat /etc/group
+echo ''
 
-
-
+echo -n "cat /etc/shadow"
+read -sp '' promptvar
+echo ''
 cat /etc/shadow
+echo ''
 
 
-
+echo -n "getent passwd"
+read -sp '' promptvar
+echo ''
 getent passwd
+echo ''
 
 
-
+echo -n "getent group"
+read -sp '' promptvar
+echo ''
 getent group
-
+echo ''
