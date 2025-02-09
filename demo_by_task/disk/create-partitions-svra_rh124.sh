657a93c7cfb39be7b254b70d0f8ce96f55dc1cc5
BrentD09 <brent.denny@lumifygroup.com>
2025-02-09 22:28:16 +1000

    Update create-partitions-svra_rh124.sh

--- demo_by_task/disk/create-partitions-svra_rh124.sh
+++ demo_by_task/disk/create-partitions-svra_rh124.sh
@@ -10,40 +10,53 @@ fi
 hostnm=$(hostname)
 if [[ $hostnm =~ servera ]]; then
   echo 'Define the disk and assign it the variable DISK  (e.g., /dev/sda)'
-  echo 'DISK="/dev/vdb"'
+  echo -n 'DISK="/dev/vdb"'
   read -sp '' promptvar
+  echo ''
   DISK="/dev/vdb"
+  echo ''
   
   # Automate partitioning of using parted
   
   diskparttype=$(parted -sm $DISK print)
   if [[ $diskparttype =~ 'unknown' ]];then 
     echo 'Create a GPT partition table'
-    echo 'parted $DISK --script mklabel gpt'
+    echo -n 'parted $DISK --script mklabel gpt'
     read -sp '' promptvar
+    echo ''
     parted $DISK --script mklabel gpt
+    echo ''
   fi
   
   echo Create the first partition of 500 MB
-  echo 'parted $DISK --script mkpart primary ext4 0% 500MB'
+  echo -n 'parted $DISK --script mkpart primary ext4 0% 500MB'
   read -sp '' promptvar
+  echo ''
   parted $DISK --script mkpart primary ext4 0% 500MB
+  echo ''
   
   echo 'Create the second partition'
-  echo 'parted $DISK --script mkpart primary ext4 500MB 2000MB'
+  echo -n 'parted $DISK --script mkpart primary ext4 500MB 2000MB'
   read -sp '' promptvar
+  echo ''
   parted $DISK --script mkpart primary ext4 500MB 2000MB
+  echo ''
   
   echo 'Print the partition table to verify'
-  echo 'parted $DISK --script print'
+  echo -n 'parted $DISK --script print'
+  read -sp '' promptvar
+  echo ''
   parted $DISK --script print
+  echo ''
 
   echo Format the partitions
   echo "Partitions created on $DISK."
-  echo "mkfs.ext4 ${DISK}1 ; mkfs.ext4 ${DISK}2"
+  echo -n "mkfs.ext4 ${DISK}1 ; mkfs.ext4 ${DISK}2"
   read -sp '' promptvar
+  echo ''
   mkfs.ext4 -F ${DISK}1
   mkfs.ext4 -F ${DISK}2
+  echo ''
 else 
   echo 1>&2 "You are on the wrong server, please do this from servera"
 fi
