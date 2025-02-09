cc9d490eadbd33b0ebdf5ed107fa7344273c49e5
BrentD09 <brent.denny@lumifygroup.com>
2025-02-09 22:51:44 +1000

    Update delete-tmpfiles_rh199.sh

--- demo_by_task/schedule/delete-tmpfiles_rh199.sh
+++ demo_by_task/schedule/delete-tmpfiles_rh199.sh
@@ -10,42 +10,50 @@ echo '' >> /etc/tmpfiles.d/myapp.conf
 echo '# Remove files that have not been accessed in 1 min' >> /etc/tmpfiles.d/myapp.conf
 echo 'x /var/tmp/myapp - - - 1m' >> /etc/tmpfiles.d/myapp.conf
 
-echo 'cat /etc/tmpfiles.d/myapp.conf'
+echo -n 'cat /etc/tmpfiles.d/myapp.conf'
 read -sp '' promptvar
+echo ''
 cat /etc/tmpfiles.d/myapp.conf
 echo ''
 
 echo 'Check to see if the myapp directory exists, it will probably not exist yet'
 echo 'tree /usr/tmp'
 read -sp '' promptvar
+echo ''
 tree /usr/tmp
 echo ''
 
 echo 'Have systemd-tmpfiles create the files and directories we need'
-echo 'systemd-tmpfiles --create'
+echo -n 'systemd-tmpfiles --create'
 read -sp '' promptvar
+echo ''
 systemd-tmpfiles --create
 echo ''
 
 echo 'Add a file to the /usr/tmp/myapp directory'
-echo 'touch /usr/tmp/myapp/demofile.txt'
+echo -n 'touch /usr/tmp/myapp/demofile.txt'
 read -sp '' promptvar
+echo ''
 touch /usr/tmp/myapp/demofile.txt
 tree /usr/tmp/myapp/
 echo ''
 
 echo 'Wait 90 seconds'
-echo 'sleep 90'
+echo -n 'sleep 90'
+read -sp '' promptvar
+echo ''
 sleep 90
 echo ''
 
 echo 'Have systemd-tmpfiles clean the temp directories '
-echo 'systemd-tmpfiles --clean'
+echo -n 'systemd-tmpfiles --clean'
 read -sp '' promptvar
+echo ''
 systemd-tmpfiles --clean
 echo ''
 
 echo 'Check the tmp directory again, demofile.txt should be deleted'
-echo 'tree /usr/tmp'
+echo -n 'tree /usr/tmp'
+echo ''
 read -sp '' promptvar
 tree /usr/tmp
