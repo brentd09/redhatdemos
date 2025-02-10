a82f1ee0f993e81ab729ccc956a499e47a4f9f26
BrentD09 <brent.denny@lumifygroup.com>
2025-02-10 14:25:44 +1000

    Update common-commands_rh199.sh

--- demo_by_task/systemd/common-commands_rh199.sh
+++ demo_by_task/systemd/common-commands_rh199.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
+#
+#me=$(whoami)
+#if [[ $me != root ]];then
+#   echo 'Usage: Must be run as root on serverb'
+#fi
+#
+#this_machine=$(hostname)
+#if [[ $this_machine != serverb ]];then
+#  echo 'Usage: Must be run as root on serverb'
+#  exit1
+#fi
+
+systemctl install -y httpd
+
+systemctl list-units --type=service
+
+# List regardless of activation state
+systemctl list-units --type=service --all
+systemctl
+systemctl list-unit-filess --type=service 
+systemctl status httpd
+systemctl enable --now httpd
+systemctl stop httpd
+systemctl start httpd
+systemctl restart httpd
+systemctl reload httpd
+# After configuring unit files and systemd configuration you need to run daemon-reload 
+systemctl daemon-reload
+systemctl is-active httpd
+systemctl is-enabled httpd
+systemctl  httpd
+
+
+
 
