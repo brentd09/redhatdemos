dc0d4aa0da2693e2ba663bbad537a3d37745c402
BrentD09 <brent.denny@lumifygroup.com>
2025-02-10 23:33:07 +1000

    Update network-manager-config_rh199.sh

--- demo_by_task/network/network-manager-config_rh199.sh
+++ demo_by_task/network/network-manager-config_rh199.sh
@@ -8,7 +8,7 @@ NETOCT=$(echo $IPADDR | grep -Po '^(\d{1,3}\.){3}')
 FTHOCT=$(echo $IPADDR | grep -Po '\d{1,3}\/' | sed 's/\///g')
 NEWFTH=$(expr $FTHOCT + 10)
 NTMASK=$(echo $IPADDR | grep -Po '\/\d{1,2}$')
-NEXTIP=$(echo ${{NETOCT}${NEWFTH}${NTMASK})
+NEXTIP=$(echo ${NETOCT}${NEWFTH}${NTMASK})
 
 nmcli device show
 nmcli device show $DEVICE 
