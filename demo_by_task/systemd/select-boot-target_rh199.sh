b57fad5e3f2e7813af00875953f4040c9abd5b7d
BrentD09 <brent.denny@lumifygroup.com>
2025-02-10 14:51:43 +1000

    Update select-boot-target_rh199.sh

--- demo_by_task/systemd/select-boot-target_rh199.sh
+++ demo_by_task/systemd/select-boot-target_rh199.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
-#
-#me=$(whoami)
-#if [[ $me != root ]];then
-#  echo 'Usage: Must be run as root on serverb'
-#fi
-#
-#this_machine=$(hostname)
-#if [[ $this_machine != servera ]];then
-#  echo 'Usage: Must be run as root on servera'
-#exit1
-#fi
 
+me=$(whoami)
+if [[ $me != root ]];then
+  echo 'Usage: Must be run as root on serverb'
+fi
+
+this_machine=$(hostname)
+if [[ $this_machine != servera ]];then
+  echo 'Usage: Must be run as root on servera'
+  exit1
+fi
+
+echo -n "systemctl get-default"
+read -sp '' promptvar
+echo ''
 systemctl get-default
-systemctl set-default graphical.target
+echo ''
+
+echo -n "systemctl set-default multi-user.target"
+read -sp '' promptvar
+echo ''
 systemctl set-default multi-user.target
-systemctl isolate multi-user.target
+echo ''
+
+echo -n "systemctl set-default graphical.target"
+read -sp '' promptvar
+echo ''
+systemctl set-default graphical.target
+echo ''
+
+echo -n "cat /usr/lib/systemd/system/graphical.target"
+read -sp '' promptvar
+echo ''
 cat /usr/lib/systemd/system/graphical.target
+echo ''
+
+echo -n "systemctl isolate multi-user.target"
+read -sp '' promptvar
+echo ''
+systemctl isolate multi-user.target
+echo ''
