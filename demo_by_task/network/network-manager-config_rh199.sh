a4316eea016c6bc94239c8d8a65e82d85209ea4a
BrentD09 <brent.denny@lumifygroup.com>
2025-02-10 23:38:53 +1000

    Update network-manager-config_rh199.sh

--- demo_by_task/network/network-manager-config_rh199.sh
+++ demo_by_task/network/network-manager-config_rh199.sh
@@ -3,6 +3,7 @@
 DEVICE=$(nmcli device show | grep GENERAL.DEVICE | grep -Pv '\blo\b$' | awk '{print $2}' )
 IPADDR=$(ip addr show $DEVICE | grep -P '\binet\b' | awk '{print $2}')
 DEFGW=$(ip route | grep default | awk '{print $3}' )
+DNSIP=$(nmcli device show $DEVICE | grep DNS | head -n 1 | awk '{print $2}')
 
 NETOCT=$(echo $IPADDR | grep -Po '^(\d{1,3}\.){3}')
 FTHOCT=$(echo $IPADDR | grep -Po '\d{1,3}\/' | sed 's/\///g')
@@ -13,3 +14,4 @@ NEXTIP=$(echo ${NETOCT}${NEWFTH}${NTMASK})
 nmcli device show
 nmcli device show $DEVICE 
 
+nmcli connection add con-name demo1 ipv4.addresses $NEWIP ip
