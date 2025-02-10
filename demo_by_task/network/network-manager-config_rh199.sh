fbaaea6423fbf3a57ee253c68aa354218318453f
BrentD09 <brent.denny@lumifygroup.com>
2025-02-10 23:03:24 +1000

    Update network-manager-config_rh199.sh

--- demo_by_task/network/network-manager-config_rh199.sh
+++ demo_by_task/network/network-manager-config_rh199.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+DEVICE=$(nmcli device show | grep GENERAL.DEVICE | grep -Pv '\blo\b$')
+IPADDR=$(ip addr show $DEVICE | grep inet | awk '{print $2}')
+DEFGW=$(ip route | grep default | awk '{print $3}' )
+
+nmcli device show
+nmcli device show $DEVICE 
+
