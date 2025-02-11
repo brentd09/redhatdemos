97ca2181225834a995190a7fe88c3db95c9ff943
BrentD09 <brent.denny@lumifygroup.com>
2025-02-11 11:34:35 +1000

    Update network-manager-permissions_rh199.sh

--- demo_by_task/network/network-manager-permissions_rh199.sh
+++ demo_by_task/network/network-manager-permissions_rh199.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo -n "sudo -u student -i nmcli gen permissions"
+read -sp '' promptvar
+echo ''
+sudo -u student -i nmcli gen permissions
+echo ''
+
 echo -n "nmcli gen permissions"
 read -sp '' promptvar
 echo ''
