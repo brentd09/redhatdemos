263c045972aa968b432b5bb6bcd9afdacd82e4c2
BrentD09 <brent.denny@lumifygroup.com>
2025-02-11 11:48:51 +1000

    Update network-manager-files_rh199.sh

--- demo_by_task/network/network-manager-files_rh199.sh
+++ demo_by_task/network/network-manager-files_rh199.sh
@@ -1,57 +1,14 @@
 #!/bin/bash
-echo -n "DEVICE=$(nmcli device show | grep GENERAL.DEVICE | grep -Pv 'blob$' | awk '{print $2}' )"
-read -sp '' promptvar
-echo ''
+
 DEVICE=$(nmcli device show | grep GENERAL.DEVICE | grep -Pv 'blob$' | awk '{print $2}' )
-echo ''
-
-echo -n "IPADDR=$(ip addr show $DEVICE | grep -P 'binetb' | head -n 1 | awk '{print $2}')"
-read -sp '' promptvar
-echo ''
 IPADDR=$(ip addr show $DEVICE | grep -P 'binetb' | head -n 1 | awk '{print $2}')
-echo ''
-
-echo -n "DEFGW=$(ip route | grep default | awk '{print $3}' )"
-read -sp '' promptvar
-echo ''
 DEFGW=$(ip route | grep default | awk '{print $3}' )
-echo ''
-
-echo -n "DNSIP=$(nmcli device show $DEVICE | grep DNS | head -n 1 | awk '{print $2}')"
-read -sp '' promptvar
-echo ''
 DNSIP=$(nmcli device show $DEVICE | grep DNS | head -n 1 | awk '{print $2}')
-echo ''
-
-echo -n "NETOCT=$(echo $IPADDR | grep -Po '^(d{1,3}.){3}')"
-read -sp '' promptvar
-echo ''
 NETOCT=$(echo $IPADDR | grep -Po '^(d{1,3}.){3}')
-echo ''
-
-echo -n "FTHOCT=$(echo $IPADDR | grep -Po 'd{1,3}/' | sed 's////g')"
-read -sp '' promptvar
-echo ''
 FTHOCT=$(echo $IPADDR | grep -Po 'd{1,3}/' | sed 's////g')
-echo ''
-
-echo -n "NEWFTH=$((10 + $FTHOCT))"
-read -sp '' promptvar
-echo ''
 NEWFTH=$((10 + $FTHOCT))
-echo ''
-
-echo -n "NTMASK=$(echo $IPADDR | grep -Po '/d{1,2}$')"
-read -sp '' promptvar
-echo ''
 NTMASK=$(echo $IPADDR | grep -Po '/d{1,2}$')
-echo ''
-
-echo -n "NEXTIP=$(echo ${NETOCT}${NEWFTH}${NTMASK})"
-read -sp '' promptvar
-echo ''
 NEXTIP=$(echo ${NETOCT}${NEWFTH}${NTMASK})
-echo ''
 
 echo -n "ls -l /etc/NetworkManager/system-connections"
 read -sp '' promptvar
