065fe1ee9ad2f4aeced3c532d3a52978bc1c4d7a
BrentD09 <brent.denny@lumifygroup.com>
2025-02-11 14:38:57 +1000

    Update grep-regex.sh

--- demo_by_task/regex/grep-regex.sh
+++ demo_by_task/regex/grep-regex.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
-
+echo -n "grep 'code' linux-poem.txt"
+read -sp '' promptvar
+echo ''
 grep 'code' linux-poem.txt
+echo ''
+
+echo -n "grep 'the' linux-poem.txt"
+read -sp '' promptvar
+echo ''
 grep 'the' linux-poem.txt
+echo ''
+
+echo -n "grep -i 'the' linux-poem.txt"
+read -sp '' promptvar
+echo ''
 grep -i 'the' linux-poem.txt
+echo ''
+
+echo -n "grep -i '^the' linux-poem.txt"
+read -sp '' promptvar
+echo ''
 grep -i '^the' linux-poem.txt
+echo ''
+
+echo -n "grep '.' linux-poem.txt"
+read -sp '' promptvar
+echo ''
 grep '.' linux-poem.txt
-grep '\.' linux-poem.txt
+echo ''
+
+echo -n "grep '.' linux-poem.txt"
+read -sp '' promptvar
+echo ''
+grep '.' linux-poem.txt
+echo ''
+
+echo -n "grep -iP '(?<=joined )the' linux-poem.txt"
+read -sp '' promptvar
+echo ''
 grep -iP '(?<=joined )the' linux-poem.txt
+echo ''
+
+echo -n "grep -iv 'code' linux-poem.txt"
+read -sp '' promptvar
+echo ''
 grep -iv 'code' linux-poem.txt
+echo ''
