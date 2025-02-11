9345aac049d5894aca3e6afe692ef82b03027876
BrentD09 <brent.denny@lumifygroup.com>
2025-02-11 23:09:24 +1000

    Update reset-snapshot.sh

--- rh-vmhost-scripts/reset-snapshot.sh
+++ rh-vmhost-scripts/reset-snapshot.sh
@@ -1,33 +1,10 @@
-virsh snapshot-create-as --domain vm1 --name snap --description 'tmp snap' --atomic
-if [[ $? -ne 0 ]]; then
-  exit 1
-fi
-virsh snapshot-create-as --domain vm2 --name snap --description 'tmp snap' --atomic
-if [[ $? -ne 0 ]]; then
-  exit 2
-fi
-sleep 10
-virsh snapshot-delete --domain vm1 --snapshotname ReadyForLabs 
-if [[ $? -ne 0 ]]; then
-  exit 3
-fi
-virsh snapshot-delete --domain vm2 --snapshotname ReadyForLabs 
-if [[ $? -ne 0 ]]; then
-  exit 4
-fi
-virsh snapshot-create-as --domain vm1 --name ReadyForLabs --description 'demo' --atomic 
-if [[ $? -ne 0 ]]; then
-  exit 5
-fi
-virsh snapshot-create-as --domain vm2 --name ReadyForLabs --description 'demo' --atomic 
-if [[ $? -ne 0 ]]; then
-  exit 6
-fi
-virsh snapshot-delete --domain vm1 --snapshotname  snap
-if [[ $? -ne 0 ]]; then
-  exit 7
-fi
-virsh snapshot-delete --domain vm2 --snapshotname  snap
-if [[ $? -ne 0 ]]; then
-  exit 8
-fi
+vmnames="vm1 vm2"
+now=$(date +"%X %d-%b-%Y")
+
+for vm in $vmnames; do
+  snaps=$(virsh snapshot-list $vm --name)
+  for snap in $snaps; do
+    virsh snapshot-delete --domain $vm --snapshotname $snap
+  done
+  virsh snapshot-create-as --domain $vm --name ReadyForLabs --description $now  --atomic 
+done  
