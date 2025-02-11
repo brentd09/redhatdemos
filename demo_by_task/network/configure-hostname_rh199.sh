27bd4659afa7f749e0242725346ccd45481f5a6a
BrentD09 <brent.denny@lumifygroup.com>
2025-02-11 12:12:51 +1000

    Update configure-hostname_rh199.sh

--- demo_by_task/network/configure-hostname_rh199.sh
+++ demo_by_task/network/configure-hostname_rh199.sh
@@ -1,16 +1,54 @@
 #!/bin/bash
-
+echo -n "THIS_MACHINE=$(hostname)"
+read -sp '' promptvar
+echo ''
 THIS_MACHINE=$(hostname)
+echo ''
 
+echo -n "hostname"
+read -sp '' promptvar
+echo ''
 hostname
+echo ''
+
+echo -n "hostname -I"
+read -sp '' promptvar
+echo ''
 hostname -I
+echo ''
+
+echo -n "hostnamectl hostname host.example.com"
+read -sp '' promptvar
+echo ''
 hostnamectl hostname host.example.com
+echo ''
+
+echo -n "hostnamectl status"
+read -sp '' promptvar
+echo ''
 hostnamectl status
+echo ''
+
+echo -n "cat /etc/hosts"
+read -sp '' promptvar
+echo ''
 cat /etc/hosts
+echo ''
+
+echo -n "cat resolv.conf"
+read -sp '' promptvar
+echo ''
 cat resolv.conf
+echo ''
+
+echo -n "dig serverb"
+read -sp '' promptvar
+echo ''
 dig serverb
+echo ''
 
-
+echo -n "hostnamectl hostname $THIS_MACHINE"
+read -sp '' promptvar
+echo ''
 hostnamectl hostname $THIS_MACHINE
-
-
+echo ''
