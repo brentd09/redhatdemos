85826220ce4c644cc46eeb923e4af1f8c0b912b2
BrentD09 <brent.denny@lumifygroup.com>
2025-02-11 11:51:07 +1000

    Update network-manager-files_rh199.sh

--- demo_by_task/network/network-manager-files_rh199.sh
+++ demo_by_task/network/network-manager-files_rh199.sh
@@ -4,11 +4,7 @@ DEVICE=$(nmcli device show | grep GENERAL.DEVICE | grep -Pv 'blob$' | awk '{prin
 IPADDR=$(ip addr show $DEVICE | grep -P 'binetb' | head -n 1 | awk '{print $2}')
 DEFGW=$(ip route | grep default | awk '{print $3}' )
 DNSIP=$(nmcli device show $DEVICE | grep DNS | head -n 1 | awk '{print $2}')
-NETOCT=$(echo $IPADDR | grep -Po '^(d{1,3}.){3}')
-FTHOCT=$(echo $IPADDR | grep -Po 'd{1,3}/' | sed 's////g')
-NEWFTH=$((10 + $FTHOCT))
-NTMASK=$(echo $IPADDR | grep -Po '/d{1,2}$')
-NEXTIP=$(echo ${NETOCT}${NEWFTH}${NTMASK})
+
 
 echo -n "ls -l /etc/NetworkManager/system-connections"
 read -sp '' promptvar
@@ -16,10 +12,10 @@ echo ''
 ls -l /etc/NetworkManager/system-connections
 echo ''
 
-echo -n "nmcli conn add con-name demo2 ipv4.addresses $NEXTIP ipv4.dns $DNSIP connection.interface-name $DEVICE type ethernet"
+echo -n "nmcli conn add con-name demo2 ipv4.addresses 192.168.10.111 ipv4.dns 192.168.10.1 connection.interface-name $DEVICE type ethernet"
 read -sp '' promptvar
 echo ''
-nmcli conn add con-name demo2 ipv4.addresses $NEXTIP ipv4.dns $DNSIP connection.interface-name $DEVICE type ethernet
+nmcli conn add con-name demo2 ipv4.addresses 192.168.10.111 ipv4.dns 192.168.10.1 connection.interface-name $DEVICE type ethernet
 echo ''
 
 echo -n "ls -l /etc/NetworkManager/system-connections"
