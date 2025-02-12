eccb2b316e4138ec8da7392cbbbdb1659b48859f
BrentD09 <brent.denny@lumifygroup.com>
2025-02-12 22:06:47 +1000

    Update recreate-snapshot.sh

--- rh-vmhost-scripts/recreate-snapshot.sh
+++ rh-vmhost-scripts/recreate-snapshot.sh
@@ -2,9 +2,12 @@ vmnames="vm1 vm2"
 now=$(date +"%X %d-%b-%Y")
 
 for vm in $vmnames; do
+  echo "Working on $vm"
   snaps=$(virsh snapshot-list $vm --name)
   for snap in $snaps; do
     virsh snapshot-delete --domain $vm --snapshotname $snap
   done
-  virsh snapshot-create-as --domain $vm --name ReadyForLabs --atomic 
+  virsh snapshot-create-as --domain $vm --name ReadyForLabs --atomic
+  echo 
+  echo
 done  
