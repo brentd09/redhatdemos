de55252982c13c39a72288f238db31324835a930
BrentD09 <brent.denny@lumifygroup.com>
2025-02-13 08:23:15 +1000

    Update control-vm-status.sh

--- rh-vmhost-scripts/control-vm-status.sh
+++ rh-vmhost-scripts/control-vm-status.sh
@@ -17,9 +17,13 @@ vms=$(virsh list --all --name | sort | grep -v '^$' | paste -sd ' ')
 
 for vm_guest_name in $vms; do 
   if [[ $1 =~ ^st ]];then
+    pid_virt_mgr=$(pgrep virt-manager)
     echo "Starting the VM: $vm_guest_name "  
     virsh start $vm_guest_name &> /dev/null
-  elif [[ $1 =~ ^sh ]];then
+    if [ -z "$pid_virt_mgr" ]; then
+      sudo virt-manager
+    fi
+elif [[ $1 =~ ^sh ]];then
     echo "Shutting down the VM: $vm_guest_name "  
     virsh shutdown $vm_guest_name &> /dev/null
   else
