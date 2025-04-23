111acb68b22fb9edc46ef07538d6f43e5e212ec1
BrentD09 <brent.denny@lumifygroup.com>
2025-04-23 10:50:40 +1000

    Update list_proces_priorities.sh

--- rhel9-demos/process/list_proces_priorities.sh
+++ rhel9-demos/process/list_proces_priorities.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo This command will show the ProcessID, ParentPID, Nice, Priority, Realtime Priority and the command name
-echo ps -eo PID,PPID,NI,PRI,RTPRIO,CMD
-ps -eo PID,PPID,NI,PRI,RTPRIO,CMD
+echo ps -eo pid,ppid,ni,pri,rtprio,cmd
+ps -eo pid,ppid,ni,pri,rtprio,cmd
