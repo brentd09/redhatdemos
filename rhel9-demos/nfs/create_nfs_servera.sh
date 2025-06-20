f7ca3baa359d02ae70c87bf7a9f6168f4838dabb
BrentD09 <brent.denny@lumifygroup.com>
2025-06-20 21:53:44 +1000

    Update create_nfs_servera.sh
    Finished the automatic build of the automounter

--- rhel9-demos/nfs/create_nfs_servera.sh
+++ rhel9-demos/nfs/create_nfs_servera.sh
@@ -2,6 +2,9 @@
 
 # set up servera as an NFS server with two NFS shares
 sudo -i
+ssh servera firewall-cmd --zone=public --add-service=mountd
+ssh servera firewall-cmd --zone=public --add-service=nfs
+ssh servera firewall-cmd --runtime-to-permanent
 ssh servera systemctl enable --now nfs-server
 ssh servera mkdir -p /new_projects/proj{1,2}
 ssh servera chmod -R 775 /new_projects
@@ -11,7 +14,9 @@ ssh servera 'echo "/new_projects/proj2 172.25.250.0/24(rw,sync)" >> /etc/exports
 ssh servera exportfs -ra
 ssh servera exportfs
 
+# setup the nfs client to do indirect wildcard
 dnf install -y autofs
-systemctl enable --now autofs
-echo 'my_projects    /etc/auto_projects_.autofs'
-# not finished yet
+systemctl enable autofs
+echo 'my_projects    /etc/auto_projects_.autofs' > /etc/auto.master.d/auto_master_projects.autofs
+echo '*    -fstype=nfs,rw    servera:/new_projects/&' > /etc/auto_projects_.autofs
+systemctl start autofs
