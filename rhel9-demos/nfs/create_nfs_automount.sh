143cd7126923c1485dfd3d803e7588dd7a281700
BrentD09 <brent.denny@lumifygroup.com>
2025-06-20 23:41:08 +1000

    Update create_nfs_automount.sh

--- rhel9-demos/nfs/create_nfs_automount.sh
+++ rhel9-demos/nfs/create_nfs_automount.sh
@@ -6,15 +6,16 @@ ssh servera firewall-cmd --zone=public --add-service=mountd
 ssh servera firewall-cmd --zone=public --add-service=nfs
 ssh servera firewall-cmd --runtime-to-permanent
 ssh servera systemctl enable --now nfs-server
-ssh servera mkdir -p /new_projects/proj{1,2}
+ssh servera mkdir -p /new_projects/proj{1,3}
 ssh servera chmod -R 775 /new_projects
 ssh servera chown -R student:student /new_projects 
 ssh servera 'echo "/new_projects/proj1 172.25.250.0/24(rw,sync)" >> /etc/exports.d/project_shares.exports'
 ssh servera 'echo "/new_projects/proj2 172.25.250.0/24(rw,sync)" >> /etc/exports.d/project_shares.exports'
+ssh servera 'echo "# /new_projects/proj3 172.25.250.0/24(rw,sync)" >> /etc/exports.d/project_shares.exports'
 ssh servera exportfs -ra
 ssh servera exportfs
 
-# setup the nfs client to do indirect wildcard
+# setup the nfs client to automount indirect with wildcard
 dnf install -y autofs
 systemctl enable autofs
 echo '/my_projects    /etc/auto_projects.autofs' > /etc/auto.master.d/auto_master_projects.autofs
