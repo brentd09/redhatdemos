f0872c63b8346bd239f9c08d5f1f3b8436e56bbb
BrentD09 <brent.denny@lumifygroup.com>
2025-06-26 15:58:29 +1000

    Update setup-demo-files-servera.sh

--- rhel9-demos/nfs/setup-demo-files-servera.sh
+++ rhel9-demos/nfs/setup-demo-files-servera.sh
@@ -3,33 +3,45 @@
 # Create 3 directories in /shares
 # Create 3 directories in /projects
 
-mkdir -p /shares/{public|it|admin} /projects/project{1..3}
+mkdir -p /shares/public
+mkdir -p /shares/it
+mkdir -p /shares/admin
+mkdir -p /projects/project1
+mkdir -p /projects/project2
+mkdir -p /projects/project3
 
 # Create files in each NFS share with its own contents
-echo 'PUBLIC' > /shares/public/readme
-echo 'IT' > /shares/it/readme
-echo 'ADMIN' > /shares/admin/readme
+echo 'PUBLIC'   > /shares/public/readme
+echo 'IT'       > /shares/it/readme
+echo 'ADMIN'    > /shares/admin/readme
 echo 'PROJECT1' > /projects/project1/readme
 echo 'PROJECT2' > /projects/project2/readme
 echo 'PROJECT3' > /projects/project3/readme
 
-chmod 1774 /shares/
-chmod 1774 /shares/{it,admin}
+chmod 1775 /shares/
+chmod 1775 /shares/{it,admin}
 chmod 1777 /shares/public
-chmod 1774 /projects /projects/project{1..3}
+chmod 1775 /projects /projects/project{1..3}
+
+chown :student /shares/
+chown :student /shares/{it,admin}
+chown :student /shares/public
+chown :student /projects /projects/project{1..3}
 
 sharefiles=$(find /shares -name readme)
 projectfiles=$(find /projects -name readme)
 for filename in $sharefiles; do
   chmod 664 $filename
+  chown :student $filename
 done
 for filename in $projectfiles; do
   chmod 664 $filename
+  chown :student $filename
 done
 
 dnf -y install nfs-utils
 
-echo '/shares/public/readme          172.25.250.0/24(rw)' >> /etc/exports
+echo '/shares/public/readme          172.25.250.0/24(rw)' > /etc/exports
 echo '/shares/it/readme              172.25.250.0/24(rw)' >> /etc/exports
 echo '/shares/admin/readme           172.25.250.0/24(rw)' >> /etc/exports
 echo '/projects/project1/readme      172.25.250.0/24(rw)' >> /etc/exports
